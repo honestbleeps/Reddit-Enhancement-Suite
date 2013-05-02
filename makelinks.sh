66320be6b5fb278cf05c5d8fffcdc9b0fd69a5ac
Scott McClaugherty <gamefreak4321@gmail.com>
2013-05-02 12:07:16 -0400

    Upgrade jQuery to 2.0.0
    Packaged jQuery has been upgraded to 2.0.0
    Uses of .live have been replaced with .on
    And a fix for some Opera silliness

--- makelinks.sh
+++ makelinks.sh
@@ -1,5 +1,5 @@
 files=("reddit_enhancement_suite.user.js" "res.css" "nightmode.css" "commentBoxes.css" \
-	"jquery-1.6.4.min.js" "jquery-fieldselection.min.js" \
+	"jquery-2.0.0.min.js" "jquery-fieldselection.min.js" \
 	"jquery.dragsort-0.4.3.min.js" "jquery.tokeninput.js" \
 	"tinycon.js" "snuownd.js" "guiders-1.2.8.js")
 paths=("Chrome" "XPI/data" "Opera" "RES.safariextension")
