honestbleeps/Reddit-Enhancement-Suite

commit d6895aa8551ab7a6ec58e31baf0988f9f4761b64

keyboard nav links should not apply to javascript: links this seems reasonable inside markdown areas. fixes #427

lib/reddit_enhancement_suite.user.js
@@ -5367,7 +5367,7 @@ modules['keyboardNav'] = {
 				this.setKeyIndex();
 			}
 			if ((this.pageType == 'comments') && (this.options.commentsLinkNumbers.value)) {
-				var links = obj.querySelectorAll('div.md a');
+				var links = obj.querySelectorAll('div.md a:not([href^="javascript:"])');
 				var annotationCount = 0;
 				for (var i=0, len=links.length; i<len; i++) {
 					if (!(links[i].classList.contains('madeVisible') ||

commit 6671fa7cd6b10a2cb48bc0358c394c7c0498a7e6

keyboard nav link annotations in comments fixed, refactored refactored keyboardNav.getCommentLinks to avoid inconsistency introduced in d6895aa which broke opening a link which followed a javascript: link

lib/reddit_enhancement_suite.user.js
@@ -5405,7 +5405,7 @@ modules['keyboardNav'] = {
 				this.setKeyIndex();
 			}
 			if ((this.pageType == 'comments') && (this.options.commentsLinkNumbers.value)) {
-				var links = obj.querySelectorAll('div.md a:not([href^="javascript:"])');
+				var links = this.getCommentLinks(obj);
 				var annotationCount = 0;
 				for (var i=0, len=links.length; i<len; i++) {
 					if (!(links[i].classList.contains('madeVisible') ||
@@ -6303,9 +6303,13 @@ modules['keyboardNav'] = {
 			}
 		}
 	},
+	getCommentLinks: function(obj) {
+		if (!obj) obj = this.keyboardLinks[this.activeIndex];
+		return obj.querySelectorAll('div.md a:not(.expando-button):not(.madeVisible):not([href^="javascript:"])');
+	},
 	commentLink: function(num) {
 		if (this.options.commentsLinkNumbers.value) {
-			var links = this.keyboardLinks[this.activeIndex].querySelectorAll('div.md a:not(.expando-button):not(.madeVisible)');
+			var links = this.getCommentLinks();
 			if (typeof(links[num]) != 'undefined') {
 				var thisLink = links[num];
 				if ((thisLink.nextSibling) && (typeof(thisLink.nextSibling.tagName) != 'undefined') && (thisLink.nextSibling.classList.contains('expando-button'))) {