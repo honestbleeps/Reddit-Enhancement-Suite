honestbleeps/Reddit-Enhancement-Suite

commit 3c9c3a31cc5a2936320adb5b1992ffe8ed22704d

Fix dashboard widgets issue, cache jQuery variables
Suggestion made in 77d42b508f37b2b55e53d607071593f2e1db9d1c by gavin19

lib/reddit_enhancement_suite.user.js
@@ -19771,37 +19771,44 @@ modules['dashboard'] = {
 			// $(thisWidget.scrim).fadeIn();
 		}
 		thisWidget.populate = function(response) {
-			var widgetContent = $(response).find('#siteTable');
-			$(widgetContent).attr('id', 'siteTable_' + thisWidget.basePath.replace(/\/|\+/g, '_'));
-			if (widgetContent.length === 2) widgetContent = widgetContent[1];
-			$(widgetContent).attr('url', thisWidget.url + '?limit=' + thisWidget.numPosts);
-			if ((widgetContent) && ($(widgetContent).html() !== '')) {
-				// widgetContent will contain HTML from Reddit's page load. No XSS here or you'd already be hit, can't call escapeHTML on this either and wouldn't help anyhow.
-				try {
-					$(thisWidget.contents).html(widgetContent);
-				} catch (e) {
-					console.log(e);
-				}
-				$(thisWidget.contents).fadeTo('fast', 1);
+			var $widgetContent = $(response).find('#siteTable');
+			$widgetContent.attr('id', 'siteTable_' + thisWidget.basePath.replace(/\/|\+/g, '_'));
+
+			if ($widgetContent.length === 2) $widgetContent = $($widgetContent[1]);
+
+			$widgetContent.attr('url', thisWidget.url + '?limit=' + thisWidget.numPosts);
+
+			var $thisWidgetContents = $(thisWidget.contents);
+
+			if (($widgetContent.length > 0) && ($widgetContent.html() !== '')) {
+				// $widgetContent will contain HTML from Reddit's page load. No XSS here or you'd already be hit, can't call escapeHTML on this either and wouldn't help anyhow.
+				// strip out any JavaScript
+				var widgetHTML = $widgetContent.html()
+						.replace(/<script(.|\s)*?\/script>/g, '');
+
+				$thisWidgetContents
+					.html(widgetHTML)
+					.fadeTo('fast', 1);
+
 				$(thisWidget.scrim).fadeOut(function(e) {
 					$(this).hide(); // make sure it is hidden in case the element isn't visible due to being on a different dashboard tab
 				})
-				$(thisWidget.stateControls).find('.updateTime').text('updated: ' + RESUtils.niceDateTime());
 			} else {
 				if (thisWidget.url.indexOf('/message/') !== -1) {
-					$(thisWidget.contents).html('<div class="widgetNoMail">No messages were found.</div>');
+					$thisWidgetContents.html('<div class="widgetNoMail">No messages were found.</div>');
 				} else {
-					$(thisWidget.contents).html('<div class="error">There were no results returned for this widget. If you made a typo, simply close the widget to delete it. If reddit is just under heavy load, try clicking refresh in a few moments.</div>');
+					$thisWidgetContents.html('<div class="error">There were no results returned for this widget. If you made a typo, simply close the widget to delete it. If reddit is just under heavy load, try clicking refresh in a few moments.</div>');
 				}
-				$(thisWidget.contents).fadeTo('fast', 1);
+
+				$thisWidgetContents.fadeTo('fast', 1);
 				$(thisWidget.scrim).fadeOut();
-				$(thisWidget.stateControls).find('.updateTime').text('updated: ' + RESUtils.niceDateTime());
 			}
+			$(thisWidget.stateControls).find('.updateTime').text('updated: ' + RESUtils.niceDateTime());
+
 			// now run watcher functions from other modules on this content...
 			RESUtils.watchers.siteTable.forEach(function(callback) {
-				if (callback) callback(widgetContent[0]);
+				if (callback) callback($widgetContent[0]);
 			});
-
 		}
 		thisWidget.error = function(xhr, err) {
 			// alert('There was an error loading data for this widget. Did you type a bad path, perhaps? Removing this widget automatically.');

commit c747295b3246f03e6f97c2a479b735dc37d79b50

reverted dashboard module to its state prior to 3c9c3a31, which broke all RES modules loading on dashboard content (e.g. user tagger, image viewer, etc)...
happy to revisit the improvements made in that commit, but it broke too much functionality and i would like to get a hotfix out that at least fixes that.

lib/reddit_enhancement_suite.user.js
@@ -19130,8 +19130,6 @@ modules['settingsNavigation'] = {
 	}
 };
 
-
-
 modules['dashboard'] = {
 	moduleID: 'dashboard',
 	moduleName: 'RES Dashboard',
@@ -19823,46 +19821,42 @@ modules['dashboard'] = {
 			modules['dashboard'].dashboardUL.prepend(thisWidget.widgetEle);
 			// $(thisWidget.scrim).fadeIn();
 		}
+
 		thisWidget.populate = function(response) {
-			var $widgetContent = $(response).find('#siteTable');
-			$widgetContent.attr('id', 'siteTable_' + thisWidget.basePath.replace(/\/|\+/g, '_'));
-
-			if ($widgetContent.length === 2) $widgetContent = $($widgetContent[1]);
-
-			$widgetContent.attr('url', thisWidget.url + '?limit=' + thisWidget.numPosts);
-
-			var $thisWidgetContents = $(thisWidget.contents);
-
-			if (($widgetContent.length > 0) && ($widgetContent.html() !== '')) {
-				// $widgetContent will contain HTML from Reddit's page load. No XSS here or you'd already be hit, can't call escapeHTML on this either and wouldn't help anyhow.
-				// strip out any JavaScript
-				var widgetHTML = $widgetContent.html()
-						.replace(/<script(.|\s)*?\/script>/g, '');
-
-				$thisWidgetContents
-					.html(widgetHTML)
-					.fadeTo('fast', 1);
-
-				$(thisWidget.scrim).fadeOut(function(e) {
-					$(this).hide(); // make sure it is hidden in case the element isn't visible due to being on a different dashboard tab
-				})
+			var widgetContent = $(response).find('#siteTable');
+	
+			$(widgetContent).attr('id', 'siteTable_' + thisWidget.basePath.replace(/\/|\+/g, '_'));
+			if (widgetContent.length === 2) widgetContent = widgetContent[1];
+			$(widgetContent).attr('url', thisWidget.url + '?limit=' + thisWidget.numPosts);
+			if ((widgetContent) && ($(widgetContent).html() !== '')) {
+			// widgetContent will contain HTML from Reddit's page load. No XSS here or you'd already be hit, can't call escapeHTML on this either and wouldn't help anyhow.
+			try {
+				$(thisWidget.contents).html(widgetContent);
+			} catch (e) {
+				console.log(e);
+			}
+			
+			$(thisWidget.contents).fadeTo('fast', 1);
+			$(thisWidget.scrim).fadeOut(function(e) {
+				$(this).hide(); // make sure it is hidden in case the element isn't visible due to being on a different dashboard tab
+			})
+			$(thisWidget.stateControls).find('.updateTime').text('updated: ' + RESUtils.niceDateTime());
 			} else {
 				if (thisWidget.url.indexOf('/message/') !== -1) {
-					$thisWidgetContents.html('<div class="widgetNoMail">No messages were found.</div>');
+					$(thisWidget.contents).html('<div class="widgetNoMail">No messages were found.</div>');
 				} else {
-					$thisWidgetContents.html('<div class="error">There were no results returned for this widget. If you made a typo, simply close the widget to delete it. If reddit is just under heavy load, try clicking refresh in a few moments.</div>');
+					$(thisWidget.contents).html('<div class="error">There were no results returned for this widget. If you made a typo, simply close the widget to delete it. If reddit is just under heavy load, try clicking refresh in a few moments.</div>');
 				}
-
-				$thisWidgetContents.fadeTo('fast', 1);
+				$(thisWidget.contents).fadeTo('fast', 1);
 				$(thisWidget.scrim).fadeOut();
+				$(thisWidget.stateControls).find('.updateTime').text('updated: ' + RESUtils.niceDateTime());
 			}
-			$(thisWidget.stateControls).find('.updateTime').text('updated: ' + RESUtils.niceDateTime());
-
 			// now run watcher functions from other modules on this content...
 			RESUtils.watchers.siteTable.forEach(function(callback) {
-				if (callback) callback($widgetContent[0]);
+				if (callback) callback(widgetContent[0]);
 			});
 		}
+
 		thisWidget.error = function(xhr, err) {
 			// alert('There was an error loading data for this widget. Did you type a bad path, perhaps? Removing this widget automatically.');
 			// modules['dashboard'].removeWidget(thisWidget.optionsObject());