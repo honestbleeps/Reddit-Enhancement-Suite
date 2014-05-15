9fca3163150e2f29fa9a307926500eaf2fa6cb5a
Kevin Ji <kevin.ji@outlook.com>
2014-05-14 21:46:19 -0700

    makelints: Reformat file
    Remove superfluous semicolons, reindent, add newlines

--- makelinks.sh
+++ makelinks.sh
@@ -9,34 +9,38 @@ paths=("Chrome" "XPI/data" "Opera" "OperaBlink" "RES.safariextension")
 for i in "${files[@]}"
 do
 	for j in "${paths[@]}"
-		do
-			if [[ -f $i ]]
-			then
-				file=$(basename $i)
-				dir=$(dirname $i)
-				if [ "$dir" == "lib/modules" ]
-				then
-					dest="./$j/modules/"
-				else
-					dest="./$j/"
-				fi
-				echo "Re-linking:" $dest$file
-				if [ -f $dest$file ];
-				then
-					rm $dest$file
-				fi
+	do
+		if [[ -f $i ]]
+		then
+			file=$(basename $i)
+			dir=$(dirname $i)
 
-				if [ "clean" != "$1" ];
-				then
-					mkdir -p $dest
-					ln ./$i $dest
-				fi
+			if [ "$dir" == "lib/modules" ]
+			then
+				dest="./$j/modules/"
+			else
+				dest="./$j/"
 			fi
-		done
+
+			echo "Re-linking:" $dest$file
+
+			if [ -f $dest$file ]
+			then
+				rm $dest$file
+			fi
+
+			if [ "clean" != "$1" ]
+			then
+				mkdir -p $dest
+				ln ./$i $dest
+			fi
+		fi
+	done
 done
 
 rm "OperaBlink/browsersupport-chrome.js"
-if [ "clean" != "$1" ];
+
+if [ "clean" != "$1" ]
 then
 	ln "Chrome/browsersupport-chrome.js" "OperaBlink/browsersupport-chrome.js"
 fi
