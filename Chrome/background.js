6b46bf5e1455427308ff43f203ad580662a2dc53
Kevin Ji <kevin.ji@outlook.com>
2014-08-06 11:23:19 -0700

    Convert 4 spaces to tabs

--- Chrome/background.js
+++ Chrome/background.js
@@ -15,18 +15,18 @@
 
 	Thanks, I appreciate your consideration.  Without further ado, the all-important GPL Statement:
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
@@ -105,8 +105,8 @@ chrome.runtime.onMessage.addListener(
 			case 'deleteCookie':
 				// Get chrome cookie handler
 				if (!chrome.cookies) {
-                    chrome.cookies = chrome.experimental.cookies;
-                }
+					chrome.cookies = chrome.experimental.cookies;
+				}
 				chrome.cookies.remove({'url': 'http://reddit.com', 'name': request.cname});
 				break;
 			case 'ajax':
