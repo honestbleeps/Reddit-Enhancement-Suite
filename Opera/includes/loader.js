c79fb84febcc96ad11fce8cb64b37d06ce665d97
andytuba <andytuba@andytuba.com>
2015-02-28 18:06:59 -0800

    modules.floater, for handling floaty UI elements
    including Never-Ending Reddit mail and pause buttons

--- Opera/includes/loader.js
+++ Opera/includes/loader.js
@@ -54,6 +54,7 @@ window.addEventListener('DOMContentLoaded', function() {
 		'modules/accountSwitcher.js',
 		'modules/betteReddit.js',
 		'modules/commandLine.js',
+		'modules/floater.js',
 		'modules/orangered.js',
 		'modules/announcements.js',
 		'modules/selectedEntry.js',
