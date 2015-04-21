c15a7b5a6453059efdb8b3b90c39066b53bbf8b8
Erik <erikdesjardins@users.noreply.github.com>
2015-04-21 16:05:25 -0400

    one object for each browser's config should make it slightly more straightforward to add new browsers

--- gulpfile.js
+++ gulpfile.js
@@ -16,59 +16,65 @@ gulp.task('default', ['clean'], function() {
 
 // Paths
 var rootBuildDir = 'dist',
-	buildFolder = {
-		chrome:  'chrome',
-		safari:  'RES.safariextension',
-		firefox: 'XPI',
-		oblink:  'oblink',
-		opera:   'opera'
+	commonFiles = ['lib/**/*.js', 'lib/**/*.json', 'lib/**/*.css', 'lib/**/*.html'],
+	config = {
+		chrome: {
+			buildFolder: 'chrome',
+			manifest: 'Chrome/manifest.json',
+			// dest is relative to the browser's buildDir [i.e. getBuildDir(browser)], src is relative to the root of the project
+			buildFiles: [
+				{ dest: 'images', src: ['Chrome/images/*.png'] },
+				{ dest: '/',      src: ['Chrome/*.js', 'Chrome/*.png', 'Chrome/*.html', 'package.json', 'Chrome/manifest.json'] },
+				{ dest: '/',      src: commonFiles }
+			]
+		},
+		safari: {
+			buildFolder: 'RES.safariextension',
+			manifest: 'RES.safariextension/Info.plist',
+			buildFiles: [
+				{ dest: '/', src: ['RES.safariextension/*.js', 'RES.safariextension/*.png', 'RES.safariextension/*.html', 'package.json', 'RES.safariextension/info.plist'] },
+				{ dest: '/', src: commonFiles }
+			]
+		},
+		firefox: {
+			buildFolder: 'XPI',
+			manifest: 'XPI/lib/main.js',
+			buildFiles: [
+				{ dest: 'data', src: ['XPI/data/**/*'] },
+				{ dest: 'data', src: commonFiles },
+				{ dest: 'lib',  src: ['XPI/lib/**/*'] },
+				{ dest: '/',    src: ['*.png', 'XPI/package.json'] }
+			]
+		},
+		oblink: {
+			buildFolder: 'oblink',
+			manifest: 'OperaBlink/manifest.json',
+			buildFiles: [
+				{ dest: 'images', src: ['OperaBlink/images/*.png'] },
+				{ dest: '/',      src: ['OperaBlink/*.js', 'Chrome/browsersupport-chrome.js', 'OperaBlink/*.png', 'OperaBlink/*.json', 'package.json'] },
+				{ dest: '/',      src: commonFiles }
+			]
+		},
+		opera: {
+			buildFolder: 'opera',
+			manifest: 'Opera/includes/loader.js',
+			buildFiles: [
+				{ dest: 'includes', src: ['Opera/includes/*.js'] },
+				{ dest: '/',        src: ['Opera/*.js', 'OperaBlink/*.gif', 'Opera/*.html', 'Opera/*.xml', 'package.json'] },
+				{ dest: '/',        src: commonFiles }
+			]
+		}
 	},
-	libFiles = ['lib/**/*.js', 'lib/**/*.json', 'lib/**/*.css', 'lib/**/*.html'],
-	// dest is relative to the browser's buildDir [i.e. getBuildDir(browser)], src is relative to the root of the project
-	buildFiles = {
-		chrome: [
-			{ dest: 'images', src: ['Chrome/images/*.png'] },
-			{ dest: '/',      src: ['Chrome/*.js', 'Chrome/*.png', 'Chrome/*.html', 'package.json', 'Chrome/manifest.json'] },
-			{ dest: '/',      src: libFiles }
-		],
-		safari: [
-			{ dest: '/', src: ['RES.safariextension/*.js', 'RES.safariextension/*.png', 'RES.safariextension/*.html', 'package.json', 'RES.safariextension/info.plist'] },
-			{ dest: '/', src: libFiles }
-		],
-		firefox: [
-			{ dest: 'data', src: ['XPI/data/**/*'] },
-			{ dest: 'data', src: libFiles },
-			{ dest: 'lib',  src: ['XPI/lib/**/*'] },
-			{ dest: '/',    src: ['*.png', 'XPI/package.json'] }
-		],
-		oblink: [
-			{ dest: 'images', src: ['OperaBlink/images/*.png'] },
-			{ dest: '/',      src: ['OperaBlink/*.js', 'Chrome/browsersupport-chrome.js', 'OperaBlink/*.png', 'OperaBlink/*.json', 'package.json'] },
-			{ dest: '/',      src: libFiles }
-		],
-		opera: [
-			{ dest: 'includes', src: ['Opera/includes/*.js'] },
-			{ dest: '/',        src: ['Opera/*.js', 'OperaBlink/*.gif', 'Opera/*.html', 'Opera/*.xml', 'package.json'] },
-			{ dest: '/',        src: libFiles }
-		]
-	},
-	manifests = {
-		chrome:  'Chrome/manifest.json',
-		safari:  'RES.safariextension/Info.plist',
-		firefox: 'XPI/lib/main.js',
-		oblink:  'OperaBlink/manifest.json',
-		opera:   'Opera/includes/loader.js'
-	},
-	// the specified `-b browser` or all browsers, if unspecified
-	selectedBrowsers = options.b ? [].concat(options.b) : Object.keys(buildFiles);
+// the `-b browser` argument(s) or all browsers, if unspecified
+	selectedBrowsers = options.b ? [].concat(options.b) : Object.keys(config);
 
 function getBuildDir(browser) {
-	return path.join(rootBuildDir, buildFolder[browser]);
+	return path.join(rootBuildDir, config[browser].buildFolder);
 }
 
 gulp.task('build', function(cb) {
 	selectedBrowsers.forEach(function(browser) {
-		buildFiles[browser].forEach(function(paths) {
+		config[browser].buildFiles.forEach(function(paths) {
 			gulp.src(paths.src)
 				.pipe(gulp.dest(path.join(getBuildDir(browser), paths.dest)));
 		});
@@ -77,10 +83,11 @@ gulp.task('build', function(cb) {
 });
 
 gulp.task('zip', function(cb) {
+	// --zipdir argument or <rootBuildDir>/zip/
 	var zipDir = options.zipdir || path.join(rootBuildDir, 'zip');
 	selectedBrowsers.forEach(function(browser) {
 		gulp.src(path.join(getBuildDir(browser), '**/*'))
-			.pipe(zip(buildFolder[browser] + '.zip'))
+			.pipe(zip(config[browser].buildFolder + '.zip'))
 			.pipe(gulp.dest(zipDir));
 	});
 	cb();
@@ -89,9 +96,9 @@ gulp.task('zip', function(cb) {
 gulp.task('watch', function() {
 	var sources = selectedBrowsers.reduce(function(previous, browser) { // combine the sources of each browser
 		return previous.concat(
-			buildFiles[browser].reduce(function(pre, cur) { // combine the sources within each browser
+			config[browser].buildFiles.reduce(function(pre, cur) { // combine the sources within each browser
 				return pre.concat(
-					cur.src.filter(function(src) { // filter out repeated directories (i.e. libFiles, chrome/oblink common files)
+					cur.src.filter(function(src) { // filter out repeated directories (i.e. commonFiles, chrome/oblink common files)
 						return previous.indexOf(src) == -1;
 					})
 				);
@@ -104,13 +111,13 @@ gulp.task('watch', function() {
 // Add new modules to browser manifests
 gulp.task('add-module', function(cb) {
 	selectedBrowsers.forEach(function(browser) {
-		addModuleToManifest(manifests[browser]);
+		addModuleToManifest(config[browser].manifest);
 	});
 	cb();
 });
 gulp.task('add-host', function(cb) {
 	selectedBrowsers.forEach(function(browser) {
-		addHostToManifest(manifests[browser]);
+		addHostToManifest(config[browser].manifest);
 	});
 	cb();
 });
