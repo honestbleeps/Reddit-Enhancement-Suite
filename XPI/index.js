4dfaba372179905ddfb5308797e67a6453194bfa
Jordan Simpson <jordan@vidly.io>
2015-09-14 09:31:05 -0700

    Deleted dupe

--- XPI/index.js
+++ XPI/index.js
@@ -233,7 +233,6 @@ pageMod.PageMod({
 		self.data.url('modules/quickMessage.js'),
 		self.data.url('modules/hosts/imgur.js'),
 		self.data.url('modules/hosts/vidly.js'),
-		self.data.url('modules/hosts/vidly.js'),
 		self.data.url('modules/hosts/twitter.js'),
 		self.data.url('modules/hosts/futurism.js'),
 		self.data.url('modules/hosts/gfycat.js'),
