172d8c23fb8ebd305c28d8e464eb0e3655fa7154
andytuba <andytuba@andytuba.com>
2015-10-18 22:10:27 -0700

    mock alert function - log.error's

--- node/lib/mocks.js
+++ node/lib/mocks.js
@@ -1,5 +1,4 @@
-/* exported location, document, sessionStorage */
-// Mocks
+/* exported location, document, sessionStorage, alert */
 
 var location = {"hash":"","search":"","pathname":"/","port":"","hostname":"www.reddit.com","host":"www.reddit.com","protocol":"https:","origin":"https://www.reddit.com","href":"https://www.reddit.com/","ancestorOrigins":{}};
 
@@ -16,7 +15,12 @@ var sessionStorage = {
 	}
 };
 
+function alert(msg) {
+	console.error('[ALERT]', msg);
+}
+
 exports.location = location;
 exports.document = document;
 exports.sessionStorage = sessionStorage;
 exports.DOMParser = require('xmldom').DOMParser;
+exports.alert = alert;
