074645ef3261fb82302387494e1a17a26d26fca5
Igor Gladkoborodov <igor.gladkoborodov@gmail.com>
2015-11-22 01:14:05 -0800

    Add Coub host

--- firefox/index.js
+++ firefox/index.js
@@ -565,6 +565,7 @@ PageMod({
 		self.data.url('modules/quickMessage.js'),
 		self.data.url('modules/hosts/imgur.js'),
 		self.data.url('modules/hosts/pornbot.js'),
+		self.data.url('modules/hosts/coub.js'),
 		self.data.url('modules/hosts/twitter.js'),
 		self.data.url('modules/hosts/futurism.js'),
 		self.data.url('modules/hosts/gfycat.js'),
