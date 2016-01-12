honestbleeps/Reddit-Enhancement-Suite

commit bd1970aaeb65ad419c77a188d43130390b7c76e1

showImages: make imageAnchor a direct link to the image instead of also linking to the post

lib/modules/showImages.js
@@ -947,7 +947,7 @@ modules['showImages'] = {
 
 			var imageAnchor = document.createElement('a');
 			imageAnchor.classList.add('madeVisible');
-			imageAnchor.href = sourceImage.href;
+			imageAnchor.href = sourceImage.src;
 			if (thisHandle.options.openInNewWindow.value) {
 				imageAnchor.target = '_blank';
 			}

commit e4ebf04e036aa2e9e5ccae59f45d33b2797c5300

Fix handling of image hrefs - images in albums link to the album
Broken in bd1970aaeb65ad419c77a188d43130390b7c76e1

lib/modules/showImages.js
@@ -988,7 +988,7 @@ addModule('showImages', function(module, moduleID) {
 
 			var imageAnchor = document.createElement('a');
 			imageAnchor.classList.add('madeVisible');
-			imageAnchor.href = sourceImage.src;
+			imageAnchor.href = sourceImage.href || sourceImage.src;
 			if (module.options.openInNewWindow.value) {
 				imageAnchor.target = '_blank';
 			}