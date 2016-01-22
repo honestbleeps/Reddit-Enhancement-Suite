044713683ce3e178529b59ead9b59bd3bf49481d
gasper <gapipro@gmail.com>
2016-01-22 10:10:47 +0100

    update livecap with new logo functionality

--- firefox/index.js
+++ firefox/index.js
@@ -569,6 +569,7 @@ PageMod({
 		self.data.url('modules/hosts/uploadly.js'),
 		self.data.url('modules/hosts/eroshare.js'),
 		self.data.url('modules/hosts/iloopit.js'),
+		self.data.url('modules/hosts/livecap.js'),
 		self.data.url('modules/hosts/twitter.js'),
 		self.data.url('modules/hosts/futurism.js'),
 		self.data.url('modules/hosts/gfycat.js'),
