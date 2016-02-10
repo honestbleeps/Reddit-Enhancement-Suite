4ecc48f501669a12e307f0d79f8a9e093fadd76f
Ignacio Ampuero <iampuero@findthebest.com>
2016-02-09 17:07:13 -0800

    Graphiq expando support

--- Firefox/index.js
+++ Firefox/index.js
@@ -231,6 +231,7 @@ pageMod.PageMod({
 		self.data.url('modules/modhelper.js'),
 		self.data.url('modules/quickMessage.js'),
 		self.data.url('modules/hosts/imgur.js'),
+		self.data.url('modules/hosts/graphiq.js'),
 		self.data.url('modules/hosts/twitter.js'),
 		self.data.url('modules/hosts/futurism.js'),
 		self.data.url('modules/hosts/gfycat.js'),
