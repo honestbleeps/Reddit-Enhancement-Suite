5884e10ad617c18c6ccbb171800fa2502b8015c5
Erik <erikdesjardins@users.noreply.github.com>
2016-04-25 18:45:06 -0400

    babel-register -> babel-core/register

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-commonjs */
 
 // This is a workaround for https://github.com/benmosher/eslint-plugin-import/issues/194
-require('babel-register');
+require('babel-core/register');
 module.exports = require('./webpack.config.babel').default;
