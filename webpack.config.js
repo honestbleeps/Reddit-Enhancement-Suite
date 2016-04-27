c52045228bf73719e03a64369f5c259ffd942947
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2016-04-27 10:17:09 -0400

    update eslint-import-resolver-webpack to 0.2.2, remove workaround

--- webpack.config.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/no-commonjs */
-
-// This is a workaround for https://github.com/benmosher/eslint-plugin-import/issues/194
-require('babel-core/register');
-module.exports = require('./webpack.config.babel').default;
