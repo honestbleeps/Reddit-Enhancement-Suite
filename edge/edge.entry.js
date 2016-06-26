5c01f12f841ffdff319c11931afcbbea00c38b4b
Erik <erikdesjardins@users.noreply.github.com>
2016-06-26 13:27:00 -0400

    Polyfill NodeList and HTMLCollection iteration for Safari and Edge

--- edge/edge.entry.js
+++ edge/edge.entry.js
@@ -5,3 +5,7 @@ if (typeof window.msBrowser !== 'undefined') {
 } else if (typeof window.browser !== 'undefined') {
 	window.chrome = window.browser; // eslint-disable-line no-native-reassign
 }
+
+// DOM Collection iteration
+NodeList.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
+HTMLCollection.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
