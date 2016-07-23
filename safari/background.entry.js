honestbleeps/Reddit-Enhancement-Suite

commit fc17a0738aa7f9a2c73decb7294c8d6808227d3f

update safari for webpack

safari/background.entry.js
@@ -4,113 +4,17 @@ import 'babel-polyfill';
 
 import Cache from '../lib/utils/Cache';
 
-const listeners = new Map();
-const waiting = new Map();
-let transaction = 0;
+import { createMessageHandler } from '../lib/environment/_helpers';
 
-/**
- * @callback MessageListener
- * @template T
- * @param {*} data The message data.
- * @param {SafariBrowserTab} tab The tab that sent the message.
- * @returns {T|Promise<T, *>} The response data, optionally wrapped in a promise.
- */
+const {
+	_handleMessage,
+	sendMessage,
+	addListener
+} = createMessageHandler((type, obj, page) => page.dispatchMessage(type, obj));
 
-/**
- * Register a listener to be invoked whenever a message of `type` is received.
- * Responses may be sent synchronously or asynchronously:
- * If `callback` returns a non-promise value, a response will be sent synchronously.
- * If `callback` returns a promise, a response will be sent asynchronously when it resolves.
- * If it rejects, an invalid response will be sent.
- * @param {string} type
- * @param {MessageListener} callback
- * @throws {Error} If a listener for `messageType` already exists.
- * @returns {void}
- */
-function addListener(type, callback) {
-	if (listeners.has(type)) {
-		throw new Error(`Listener for message type: ${type} already exists.`);
-	}
-	listeners.set(type, { callback });
-}
-
-/**
- * Sends a message to the content script proxy `page`.
- * @param {string} type
- * @param {SafariWebPageProxy} page
- * @param {*} [data]
- * @returns {Promise<*, Error>} Rejects if an invalid response is received,
- * resolves with the response data otherwise.
- */
-function sendMessage(type, page, data) {
-	++transaction;
-
-	page.dispatchMessage(type, { data, transaction });
-
-	return new Promise((resolve, reject) => waiting.set(transaction, { resolve, reject }));
-}
-
-function nonNull(callback) {
-	return new Promise(resolve => {
-		(function repeat() {
-			const val = callback;
-			if (!val) {
-				setTimeout(repeat, 1);
-				return;
-			}
-			resolve(val);
-		})();
-	});
-}
-
-safari.application.addEventListener('message', ({ name: type, message: { data, transaction, error, isResponse }, target: tab }) => {
-	if (isResponse) {
-		if (!waiting.has(transaction)) {
-			throw new Error(`No response handler for type: ${type}, transaction: ${transaction} - this should never happen.`);
-		}
-
-		const handler = waiting.get(transaction);
-		waiting.delete(transaction);
-
-		if (error) {
-			handler.reject(new Error(`Error in foreground handler for type: ${type} - message: ${error}`));
-		} else {
-			handler.resolve(data);
-		}
-
-		return;
-	}
-
-	if (!listeners.has(type)) {
-		throw new Error(`Unrecognised message type: ${type}`);
-	}
-	const listener = listeners.get(type);
-
-	async function sendResponse({ data, error }) {
-		// this is ridiculous, Safari.
-		(await nonNull(() => tab.page)).dispatchMessage(type, { data, transaction, error, isResponse: true });
-	}
-
-	let response;
-
-	try {
-		response = listener.callback(data, tab);
-	} catch (e) {
-		sendResponse({ error: e.message || e });
-		throw e;
-	}
-
-	if (response instanceof Promise) {
-		response
-			.then(data => sendResponse({ data }))
-			.catch(e => {
-				sendResponse({ error: e.message || e });
-				throw e;
-			});
-		return;
-	}
-	sendResponse({ data: response });
-}, false);
+safari.application.addEventListener('message', ({ name: type, message: obj, target: tab }) => {
+	_handleMessage(type, obj, tab.page);
+});
 
 // Listeners
 
@@ -257,6 +161,6 @@ addListener('multicast', async (request, senderTab) =>
 			.map(w => Array.from(w.tabs))
 			.reduce((acc, tabs) => acc.concat(tabs), [])
 			.filter(tab => tab !== senderTab && tab.private === senderTab.private && tab.page)
-			.map(({ page }) => sendMessage('multicast', page, request))
+			.map(({ page }) => sendMessage('multicast', request, page))
 	)
 );

lib/main.entry.js
@@ -1,27 +1,5 @@
-/* global safari */
-
 import 'babel-polyfill';
+
 import { init } from './core';
-
-if (process.env.BUILD_TARGET === 'safari') {
-	// Safari has a ridiculous bug that causes it to lose access to safari.self.tab if you click the back button.
-	// this stupid one liner fixes that.
-	window.onunload = () => {};
-
-	// since safari's built in extension stylesheets are treated as user stylesheets,
-	// we can't inject them that way.  That makes them "user stylesheets" which would make
-	// them require !important everywhere - we don't want that, so we'll inject this way instead.
-
-	const cssFiles = ['css/res.css', 'vendor/guiders.css', 'vendor/tokenize.css'];
-
-	init.headReady.then(() => {
-		cssFiles.forEach(filename => {
-			const linkTag = document.createElement('link');
-			linkTag.rel = 'stylesheet';
-			linkTag.href = safari.extension.baseURI + filename;
-			document.head.appendChild(linkTag);
-		});
-	});
-}
 
 init.init();

safari/Info.plist
@@ -7,21 +7,21 @@
 	<key>Builder Version</key>
 	<string>10601.3.9</string>
 	<key>CFBundleDisplayName</key>
-	<string>Reddit Enhancement Suite</string>
+	<string>{{prop?title!../package.json}}</string>
 	<key>CFBundleIdentifier</key>
 	<string>com.honestbleeps.redditenhancementsuite</string>
 	<key>CFBundleInfoDictionaryVersion</key>
 	<string>6.0</string>
 	<key>CFBundleShortVersionString</key>
-	<string>0.9000.1</string>
+	<string>{{prop?version!../package.json}}</string>
 	<key>CFBundleVersion</key>
-	<string>0.9000.1</string>
+	<string>{{prop?version!../package.json}}</string>
 	<key>Chrome</key>
 	<dict>
 		<key>Database Quota</key>
 		<real>10485760</real>
 		<key>Global Page</key>
-		<string>background-safari.html</string>
+		<string>{{./background-safari.html}}</string>
 	</dict>
 	<key>Content</key>
 	<dict>
@@ -29,155 +29,12 @@
 		<dict>
 			<key>Start</key>
 			<array>
-				<string>vendor/polyfill.min.js</string>
-				<string>vendor/jquery-1.11.3.min.js</string>
-				<string>vendor/guiders.js</string>
-				<string>vendor/jquery.sortable-0.9.12.js</string>
-				<string>vendor/jquery.edgescroll-0.1.js</string>
-				<string>vendor/jquery-fieldselection.min.js</string>
-				<string>vendor/favico.js</string>
-				<string>vendor/jquery.tokeninput.js</string>
-				<string>vendor/HTMLPasteurizer.js</string>
-				<string>vendor/snudown.js</string>
-				<string>vendor/hogan-3.0.2.js</string>
-				<string>core/utils.js</string>
-				<string>browsersupport.js</string>
-				<string>browsersupport-safari.js</string>
-				<string>core/options.js</string>
-				<string>core/alert.js</string>
-				<string>core/migrate.js</string>
-				<string>core/template.js</string>
-				<string>core/metadata.js</string>
-				<string>vendor/konami.js</string>
-				<string>modules/spoilerTags.js</string>
-				<string>modules/submitIssue.js</string>
-				<string>modules/betteReddit.js</string>
-				<string>modules/userTagger.js</string>
-				<string>modules/keyboardNav.js</string>
-				<string>modules/commandLine.js</string>
-				<string>modules/multiredditNavbar.js</string>
-				<string>modules/xPostLinks.js</string>
-				<string>modules/messageMenu.js</string>
-				<string>modules/easterEgg.js</string>
-				<string>modules/pageNavigator.js</string>
-				<string>modules/userInfo.js</string>
-				<string>modules/presets.js</string>
-				<string>modules/onboarding.js</string>
-				<string>modules/customToggles.js</string>
-				<string>modules/floater.js</string>
-				<string>modules/orangered.js</string>
-				<string>modules/announcements.js</string>
-				<string>modules/selectedEntry.js</string>
-				<string>modules/settingsConsole.js</string>
-				<string>modules/menu.js</string>
-				<string>modules/about.js</string>
-				<string>modules/hover.js</string>
-				<string>modules/subredditTagger.js</string>
-				<string>modules/singleClick.js</string>
-				<string>modules/commentPreview.js</string>
-				<string>modules/commentTools.js</string>
-				<string>modules/sourceSnudown.js</string>
-				<string>modules/usernameHider.js</string>
-				<string>modules/showImages.js</string>
-				<string>modules/showKarma.js</string>
-				<string>modules/hideChildComments.js</string>
-				<string>modules/showParent.js</string>
-				<string>modules/neverEndingReddit.js</string>
-				<string>modules/saveComments.js</string>
-				<string>modules/userHighlight.js</string>
-				<string>modules/nightMode.js</string>
-				<string>modules/styleTweaks.js</string>
-				<string>modules/stylesheet.js</string>
-				<string>modules/userbarHider.js</string>
-				<string>modules/accountSwitcher.js</string>
-				<string>modules/filteReddit.js</string>
-				<string>modules/newCommentCount.js</string>
-				<string>modules/spamButton.js</string>
-				<string>modules/commentNavigator.js</string>
-				<string>modules/subredditManager.js</string>
-				<string>modules/RESTips.js</string>
-				<string>modules/settingsNavigation.js</string>
-				<string>modules/dashboard.js</string>
-				<string>modules/notifications.js</string>
-				<string>modules/subredditInfo.js</string>
-				<string>modules/commentHidePersistor.js</string>
-				<string>modules/troubleshooter.js</string>
-				<string>modules/backupAndRestore.js</string>
-				<string>modules/localDate.js</string>
-				<string>modules/context.js</string>
-				<string>modules/noParticipation.js</string>
-				<string>modules/searchHelper.js</string>
-				<string>modules/submitHelper.js</string>
-				<string>modules/logoLink.js</string>
-				<string>modules/voteEnhancements.js</string>
-				<string>modules/upload.js</string>
-				<string>modules/tableTools.js</string>
-				<string>modules/modhelper.js</string>
-				<string>modules/quickMessage.js</string>
-				<string>modules/hosts/imgur.js</string>
-				<string>modules/hosts/reddituploads.js</string>
-				<string>modules/hosts/derpibooru.js</string>
-				<string>modules/hosts/wikipedia.js</string>
-				<string>modules/hosts/strawpoll.js</string>
-				<string>modules/hosts/twimg.js</string>
-				<string>modules/hosts/xkcd.js</string>
-				<string>modules/hosts/dailymotion.js</string>
-				<string>modules/hosts/jsfiddle.js</string>
-				<string>modules/hosts/gyazo.js</string>
-				<string>modules/hosts/pornbot.js</string>
-				<string>modules/hosts/coub.js</string>
-				<string>modules/hosts/uploadly.js</string>
-				<string>modules/hosts/eroshare.js</string>
-				<string>modules/hosts/iloopit.js</string>
-				<string>modules/hosts/livecap.js</string>
-				<string>modules/hosts/graphiq.js</string>
-				<string>modules/hosts/spotify.js</string>
-				<string>modules/hosts/pixpit.js</string>
-				<string>modules/hosts/twitter.js</string>
-				<string>modules/hosts/futurism.js</string>
-				<string>modules/hosts/gfycat.js</string>
-				<string>modules/hosts/gifyoutube.js</string>
-				<string>modules/hosts/vidble.js</string>
-				<string>modules/hosts/fitbamob.js</string>
-				<string>modules/hosts/giflike.js</string>
-				<string>modules/hosts/ctrlv.js</string>
-				<string>modules/hosts/snag.js</string>
-				<string>modules/hosts/picshd.js</string>
-				<string>modules/hosts/minus.js</string>
-				<string>modules/hosts/fiveHundredPx.js</string>
-				<string>modules/hosts/flickr.js</string>
-				<string>modules/hosts/steampowered.js</string>
-				<string>modules/hosts/deviantart.js</string>
-				<string>modules/hosts/tumblr.js</string>
-				<string>modules/hosts/memecrunch.js</string>
-				<string>modules/hosts/imgflip.js</string>
-				<string>modules/hosts/livememe.js</string>
-				<string>modules/hosts/makeameme.js</string>
-				<string>modules/hosts/memegen.js</string>
-				<string>modules/hosts/redditbooru.js</string>
-				<string>modules/hosts/youtube.js</string>
-				<string>modules/hosts/vimeo.js</string>
-				<string>modules/hosts/soundcloud.js</string>
-				<string>modules/hosts/clyp.js</string>
-				<string>modules/hosts/memedad.js</string>
-				<string>modules/hosts/ridewithgps.js</string>
-				<string>modules/hosts/photobucket.js</string>
-				<string>modules/hosts/giphy.js</string>
-				<string>modules/hosts/streamable.js</string>
-				<string>modules/hosts/qwipit.js</string>
-				<string>modules/hosts/raddit.js</string>
-				<string>modules/hosts/pastebin.js</string>
-				<string>modules/hosts/github.js</string>
-				<string>modules/hosts/onedrive.js</string>
-				<string>modules/hosts/oddshot.js</string>
-				<string>modules/hosts/miiverse.js</string>
-				<string>modules/hosts/swirl.js</string>
-				<string>core/init.js</string>
+				<string>{{../lib/main.entry.js}}</string>
 			</array>
 		</dict>
 	</dict>
 	<key>Description</key>
-	<string>Reddit Enhancement Suite is a collection of tools to enhance your Reddit browsing experience</string>
+	<string>{{prop?description!../package.json}}</string>
 	<key>DeveloperIdentifier</key>
 	<string>63DT2C85Z6</string>
 	<key>ExtensionInfoDictionaryVersion</key>

safari/background-safari.html
@@ -33,7 +33,6 @@
 -->
 <html>
 <head>
-	<script src="vendor/polyfill.min.js"></script>
-	<script src="background.js"></script>
+	<script src="./background.entry.js"></script>
 </head>
 </html>

safari/environment.js
@@ -2,7 +2,24 @@
 /* eslint-disable import/export */
 /* global safari */
 
+import resCss from '../lib/css/res.scss';
+
 import { createMessageHandler } from '../lib/environment/_helpers';
+import { init } from '../lib/core';
+
+// Safari has a ridiculous bug that causes it to lose access to safari.self.tab if you click the back button.
+// this stupid one liner fixes that.
+window.onunload = () => {};
+
+// since safari's built in extension stylesheets are treated as user stylesheets,
+// we can't inject them that way.  That makes them "user stylesheets" which would make
+// them require !important everywhere - we don't want that, so we'll inject this way instead.
+init.headReady.then(() => {
+	const linkTag = document.createElement('link');
+	linkTag.rel = 'stylesheet';
+	linkTag.href = safari.extension.baseURI + resCss;
+	document.head.appendChild(linkTag);
+});
 
 export * from '../lib/environment';
 

commit 60f60bc30ee45c3349a25dacf3c990ccd18cf935

New feature: Safari extension now supports Safari web browser (only on digital computers atm)
There was previously a fatal bug that caused the RES Safari extension to not work at all. Specifically, the extension would crash if it ever tried to open a non-Reddit URL in a new tab. The extension was guaranteed to do this 100% of the time thanks to the `onboarding` module, which automatically tries to open http://redditenhancementsuite.com/... in a new tab when RES starts up. Normally it only does this on first install, but since the extension would crash before it could record it had been run, it would try to do this every single time the extension tried to run, leading to a crash every single time.

The root cause of the crash was in the global page's 'openNewTabs` handler. It assumed that it was being passed a browser tab object when it was called by the global page message event handler function, but it was actually being passed a web page object. This led to the openNewTabs handler trying to call a non-existent function on that object, which created an unhandled exception that crashed the global page, breaking the extension entirely.

This commit fixes the bug by having the message event handler pass the browser tab object to handlers it calls, rather than web page objects. This object is much more useful, and is necessary for functions like openNewTabs to work. I also had to make a corresponding change to the `multicast` handler, so that it knew to expect a tab, not a web page, when dealing with message events.

I suspect the bug was introduced in fc17a0738aa, but I haven't done a bisect to verify that theory.

safari/background.entry.js
@@ -8,10 +8,10 @@ const {
 	_handleMessage,
 	sendMessage,
 	addListener,
-} = createMessageHandler((type, obj, page) => page.dispatchMessage(type, obj));
+} = createMessageHandler((type, obj, tab) => tab.page.dispatchMessage(type, obj));
 
 safari.application.addEventListener('message', ({ name: type, message: obj, target: tab }) => {
-	_handleMessage(type, obj, tab.page);
+	_handleMessage(type, obj, tab);
 });
 
 // Listeners
@@ -125,6 +125,6 @@ addListener('multicast', async (request, senderTab) =>
 			.map(w => Array.from(w.tabs))
 			.reduce((acc, tabs) => acc.concat(tabs), [])
 			.filter(tab => tab !== senderTab && tab.private === senderTab.private && tab.page)
-			.map(({ page }) => sendMessage('multicast', request, page))
+			.map(tab => sendMessage('multicast', request, tab))
 	)
 );