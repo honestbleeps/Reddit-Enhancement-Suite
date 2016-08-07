b7bf5082c3c1aed287fcdc77015e17dd46270900
Ben McGarry <BenMcGarry@users.noreply.github.com>
2016-08-07 18:32:58 +0100

    Code cleanup - EXPERIMENTAL

--- edge/edge.entry.js
+++ edge/edge.entry.js
@@ -1,10 +1,6 @@
 /* Microsoft Edge Support */
 
-if (typeof window.msBrowser !== 'undefined') {
-	window.chrome = window.msBrowser; // eslint-disable-line no-native-reassign
-} else if (typeof window.browser !== 'undefined') {
-	window.chrome = window.browser; // eslint-disable-line no-native-reassign
-}
+window.chrome = window.browser; // eslint-disable-line no-native-reassign
 
 // DOM Collection iteration
 NodeList.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
