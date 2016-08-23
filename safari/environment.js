1b607d01065a01664f070d9aec386084ce633e31
andytuba <andytuba@andytuba.com>
2016-08-23 13:07:35 -0700

    use Init.headReady instead of Safari rolling its own
    Fixes #2858

--- safari/environment.js
+++ safari/environment.js
@@ -3,6 +3,7 @@
 import resCss from '../lib/css/res.scss';
 
 import { createMessageHandler } from '../lib/environment/_messaging';
+import * as Init from '../lib/core/init';
 import { nonNull } from '../lib/utils';
 
 // DOM Collection iteration
@@ -16,12 +17,12 @@ window.onunload = () => {};
 // since safari's built in extension stylesheets are treated as user stylesheets,
 // we can't inject them that way.  That makes them "user stylesheets" which would make
 // them require !important everywhere - we don't want that, so we'll inject this way instead.
-nonNull(() => document.head, 100).then(() => {
+nonNull(() => Init.headReady, 30).then(() => Init.headReady.then(() => {
 	const linkTag = document.createElement('link');
 	linkTag.rel = 'stylesheet';
 	linkTag.href = safari.extension.baseURI + resCss;
 	document.head.appendChild(linkTag);
-});
+}));
 
 const {
 	_handleMessage,
