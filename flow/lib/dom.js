09c7762880dbfffb76903a34a767ade672487d18
Erik <erikdesjardins@users.noreply.github.com>
2016-08-23 13:42:59 -0400

    type utils/*

---
+++ flow/lib/dom.js
@@ -0,0 +1,9 @@
+type MutationObserverInit = {
+	childList?: boolean,
+	attributes?: boolean,
+	characterData?: boolean,
+	subtree?: boolean,
+	attributeOldValue?: boolean,
+	characterDataOldValue?: boolean,
+	attributeFilter?: Array<string>,
+};
---
+++ flow/stub/mustache.js
@@ -0,0 +1,3 @@
+/* @flow */
+
+export default (context?: { +[key: string]: mixed }, partials?: { +[key: string]: mixed }, indent?: string): string => '';
