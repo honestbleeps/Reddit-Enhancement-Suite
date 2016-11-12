88a8b79ce2af8c714c8ec366c992a4d65a57687e
Erik <erikdesjardins@users.noreply.github.com>
2016-11-12 17:19:46 -0500

    test for getting source of self posts

--- tests/sourceSnudown.js
+++ tests/sourceSnudown.js
@@ -1,4 +1,14 @@
 module.exports = {
+	'source of self posts': browser => {
+		browser
+			.url('https://www.reddit.com/r/RESIntegrationTests/comments/5cmspt/')
+			.waitForElementVisible('#siteTable', 1000)
+			.click('.thing.link .viewSource a')
+			.waitForElementVisible(`.thing.link .viewSource textarea`, 5000)
+			.assert.containsText(`.thing.link .viewSource textarea`, 'Self post text')
+			.end();
+
+	},
 	'can use source button more than once on a page': browser => {
 		const first = '.thing.id-t1_d9xbmol';
 		const second = '.thing.id-t1_d9xbmrf';
