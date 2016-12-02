07e20bfd853cd80cacc72e43b0be162f0c566ad3
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2016-12-01 23:21:43 -0500

    link to diff in firefox package (#3698)

---
+++ build/getChanges.js
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-commonjs, import/no-nodejs-modules */
+
+const { execSync } = require('child_process');
+const semver = require('semver');
+const { repository } = require('../package.json');
+const isBetaVersion = require('./isBetaVersion');
+
+const currentTag = execSync('git describe', { encoding: 'utf8' }).trim();
+
+const lastStableTag = execSync('git tag -l', { encoding: 'utf8' })
+	.trim()
+	.split(/[\r\n]+/)
+	.map(s => s.trim())
+	.filter(s => semver.valid(s))
+	.reverse()
+	.find(v => !isBetaVersion(v));
+
+module.exports = `https://github.com/${repository.username}/${repository.repository}/compare/${lastStableTag}...${currentTag}`;
