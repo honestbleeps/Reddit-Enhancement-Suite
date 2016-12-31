1e40efca997a154d7d5b8674585df801fcc2cec4
Erik <erikdesjardins@users.noreply.github.com>
2016-12-31 18:04:18 -0500

    xPostLinks: limit the length of subreddit names

--- tests/xPostLinks.js
+++ tests/xPostLinks.js
@@ -6,4 +6,18 @@ module.exports = {
 			.assert.containsText('#thing_t3_5lc3yw .tagline', 'x-posted from /r/Enhancement')
 			.end();
 	},
+	'subreddit too long': browser => {
+		browser
+			// /r/ style
+			.url('https://www.reddit.com/r/RESIntegrationTests/comments/5lc6gz/subreddit_name_too_long_xpost_rthis_subreddit/')
+			.waitForElementVisible('#thing_t3_5lc6gz', () => {
+				browser.expect.element('#thing_t3_5lc6gz .tagline').text.not.contain('x-posted from');
+			})
+			// implicit `(xposted from enhancement)` style
+			.url('https://www.reddit.com/r/RESIntegrationTests/comments/5lc74s/xpost_this_subreddit_name_is_too_long/')
+			.waitForElementVisible('#thing_t3_5lc74s', () => {
+				browser.expect.element('#thing_t3_5lc74s .tagline').text.not.contain('x-posted from');
+			})
+			.end();
+	},
 };
