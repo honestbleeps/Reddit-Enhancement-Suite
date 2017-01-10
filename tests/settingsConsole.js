586b4d2153e3a38542b156b5ff3d094b88c1ab21
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2017-01-10 17:08:20 -0500

    Reordering a table option stages options (#3874)
    * tests for changing/staging options
    
    * use <button> instead of <input type="button"> for "+add row"
    
    This is already used for builder options, and it makes more sense (and the main reason: it works in integration tests).
    
    * settingsConsole: reordering tables should not mutate option values directly
    ...because then the stage can't tell if the option has been changed
    
    * test for dragging to reorder table options

--- tests/settingsConsole.js
+++ tests/settingsConsole.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-expressions */
+
 module.exports = {
 	'opens on links to #res:settings': browser => {
 		browser
@@ -24,6 +26,138 @@ module.exports = {
 			.waitForElementVisible('#RESConsoleContainer')
 			.keys([browser.Keys.ESCAPE])
 			.waitForElementNotVisible('#RESConsoleContainer', 1000)
+			.end();
+	},
+	'change boolean option': browser => {
+		browser
+			.url('https://www.reddit.com/#res:settings/accountSwitcher')
+			.waitForElementVisible('#RESConsoleContainer')
+
+			// initial state, no options changed
+			.assert.cssClassPresent('#moduleOptionsSave', 'optionsSaved', 'options not staged')
+
+			// enable keepLoggedIn
+			.click('#keepLoggedInContainer')
+			.assert.cssClassNotPresent('#moduleOptionsSave', 'optionsSaved', 'options staged')
+
+			// click save
+			.click('#moduleOptionsSave')
+			.assert.cssClassPresent('#moduleOptionsSave', 'optionsSaved', 'options not staged')
+
+			// refresh and ensure that the option was saved
+			.refresh()
+			.waitForElementVisible('#RESConsoleContainer')
+			.perform(() => {
+				browser.expect.element('#keepLoggedIn').selected;
+			})
+			.end();
+	},
+	'change enum option': browser => {
+		browser
+			.url('https://www.reddit.com/#res:settings/accountSwitcher')
+			.waitForElementVisible('#RESConsoleContainer')
+
+			// initial state, no options changed
+			.assert.cssClassPresent('#moduleOptionsSave', 'optionsSaved', 'options not staged')
+			.perform(() => {
+				browser.expect.element('#dropDownStyle-0').selected;
+			})
+
+			// select "simple arrow" dropdown style
+			.click('#dropDownStyle-1')
+			.assert.cssClassNotPresent('#moduleOptionsSave', 'optionsSaved', 'options staged')
+
+			// click save
+			.click('#moduleOptionsSave')
+			.assert.cssClassPresent('#moduleOptionsSave', 'optionsSaved', 'options not staged')
+
+			// refresh and ensure that the option was saved
+			.refresh()
+			.waitForElementVisible('#RESConsoleContainer')
+			.perform(() => {
+				browser.expect.element('#dropDownStyle-1').selected;
+			})
+			.end();
+	},
+	'change text option': browser => {
+		browser
+			.url('https://www.reddit.com/#res:settings/quickMessage')
+			.waitForElementVisible('#RESConsoleContainer')
+
+			// initial state, no options changed
+			.assert.cssClassPresent('#moduleOptionsSave', 'optionsSaved', 'options not staged')
+
+			// set a value for defaultSubject
+			.setValue('#defaultSubject', ['test subject'])
+			.assert.cssClassNotPresent('#moduleOptionsSave', 'optionsSaved', 'options staged')
+
+			// click save
+			.click('#moduleOptionsSave')
+			.assert.cssClassPresent('#moduleOptionsSave', 'optionsSaved', 'options not staged')
+
+			// refresh and ensure that the option was saved
+			.refresh()
+			.waitForElementVisible('#RESConsoleContainer')
+			.assert.value('#defaultSubject', 'test subject')
+			.end();
+	},
+	'change table option': browser => {
+		browser
+			.url('https://www.reddit.com/#res:settings/accountSwitcher')
+			.waitForElementVisible('#RESConsoleContainer')
+
+			// initial state, no options changed
+			.assert.cssClassPresent('#moduleOptionsSave', 'optionsSaved', 'options not staged')
+
+			// add row
+			.click('#optionContainer-accountSwitcher-accounts .addRowButton')
+			.setValue('#accounts_accountSwitcherUsername_1', ['test'])
+			.assert.cssClassNotPresent('#moduleOptionsSave', 'optionsSaved', 'options staged')
+
+			// click save
+			.click('#moduleOptionsSave')
+			.assert.cssClassPresent('#moduleOptionsSave', 'optionsSaved', 'options not staged')
+
+			// refresh and ensure that the option was saved
+			.refresh()
+			.waitForElementVisible('#RESConsoleContainer')
+			.assert.value('#accounts_accountSwitcherUsername_0', 'test')
+			.end();
+	},
+	'drag to reorder table options': browser => {
+		if (browser.options.desiredCapabilities.browserName === 'firefox') {
+			// geckodriver doesn't support moveto https://github.com/mozilla/geckodriver/issues/159
+			browser.end();
+			return;
+		}
+
+		browser
+			.url('https://www.reddit.com/#res:settings/accountSwitcher')
+			.waitForElementVisible('#RESConsoleContainer')
+
+			// add rows
+			.click('#optionContainer-accountSwitcher-accounts .addRowButton')
+			.click('#optionContainer-accountSwitcher-accounts .addRowButton')
+			.setValue('#accounts_accountSwitcherUsername_1', ['first'])
+			.setValue('#accounts_accountSwitcherUsername_2', ['second'])
+			.click('#moduleOptionsSave')
+
+			// reorder
+			.refresh()
+			.waitForElementVisible('#RESConsoleContainer')
+			.assert.cssClassPresent('#moduleOptionsSave', 'optionsSaved', 'options not staged')
+			.moveToElement('#optionContainer-accountSwitcher-accounts tr:nth-child(1) .handle', 0, 0)
+			.mouseButtonDown()
+			.moveTo(null, 0, 50)
+			.mouseButtonUp()
+			.assert.cssClassNotPresent('#moduleOptionsSave', 'optionsSaved', 'options staged')
+
+			// ensure that changes get saved
+			.click('#moduleOptionsSave')
+			.refresh()
+			.waitForElementVisible('#RESConsoleContainer')
+			.assert.value('#accounts_accountSwitcherUsername_0', 'second')
+			.assert.value('#accounts_accountSwitcherUsername_1', 'first')
 			.end();
 	},
 };
