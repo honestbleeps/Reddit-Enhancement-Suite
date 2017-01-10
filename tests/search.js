a55b4e22e5ba28329322e5b368d1039e87d70e93
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2017-01-09 23:32:58 -0500

    Fix command line settings search (#3871)
    * fix opening search from commandLine
    ...use `input` instead of `keydown` (input is more correct anyways)
    
    AFAICT, this is what was happening:
    1. type "settings test" in the command line
    2. press and release ENTER, the submit event is fired in the command line
    3. synchronously, the settings console opens, searching for "test" and focuses the search box
    4. from the same ENTER keypress, the keyup event is fired in the (empty) search box
    5. the settings console searches for the contents of the search box (empty)
    
    * tests for commandLine settings search
    
    * skip tests in firefox

--- tests/search.js
+++ tests/search.js
@@ -1,9 +1,26 @@
 module.exports = {
 	'basic functionality': browser => {
 		browser
-			.url('https://www.reddit.com/wiki/pages#res:settings/search/testEnvironment')
+			.url('https://en.reddit.com/wiki/pages#res:settings/search/testEnvironment')
 			.waitForElementVisible('#RESConsoleContainer')
 			.waitForElementVisible('#SearchRES-results')
+			.assert.containsText('#SearchRES-results', 'Test Environment')
+			.end();
+	},
+	'opening from command line': browser => {
+		if (browser.options.desiredCapabilities.browserName === 'firefox') {
+			// geckodriver doesn't support elementSendKeys https://github.com/mozilla/geckodriver/issues/159
+			browser.end();
+			return;
+		}
+
+		browser
+			.url('https://en.reddit.com/wiki/pages/')
+			.waitForElementVisible('#RESSettingsButton')
+			.keys(['.'])
+			.setValue('#keyCommandInput', ['settings testEnvironment', browser.Keys.ENTER])
+			.assert.visible('#RESConsoleContainer')
+			.assert.visible('#SearchRES-results')
 			.assert.containsText('#SearchRES-results', 'Test Environment')
 			.end();
 	},
