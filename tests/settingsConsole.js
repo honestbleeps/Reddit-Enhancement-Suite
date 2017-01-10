69166a24fa3a97ae7c3f84639120eacc88a9a9a5
Erik <erikdesjardins@users.noreply.github.com>
2017-01-10 13:38:10 -0500

    tests for changing/staging options

--- tests/settingsConsole.js
+++ tests/settingsConsole.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-expressions */
+
 module.exports = {
 	'opens on links to #res:settings': browser => {
 		browser
@@ -24,6 +26,102 @@ module.exports = {
 			.waitForElementVisible('#RESConsoleContainer')
 			.keys([browser.Keys.ESCAPE])
 			.waitForElementNotVisible('#RESConsoleContainer', 1000)
+			.end();
+	},
+	'change boolean option': browser => {
+		browser
+			.url('https://www.reddit.com/#res:settings/accountSwitcher')
+			.waitForElementVisible('#RESConsoleContainer')
+
+			// initial state, no options changed
+			.assert.cssClassPresent('#moduleOptionsSave', 'optionsSaved', 'options not staged')
+
+			// enable keepLoggedIn
+			.click('#keepLoggedInContainer')
+			.assert.cssClassNotPresent('#moduleOptionsSave', 'optionsSaved', 'options staged')
+
+			// click save
+			.click('#moduleOptionsSave')
+			.assert.cssClassPresent('#moduleOptionsSave', 'optionsSaved', 'options not staged')
+
+			// refresh and ensure that the option was saved
+			.refresh()
+			.waitForElementVisible('#RESConsoleContainer')
+			.perform(() => {
+				browser.expect.element('#keepLoggedIn').selected;
+			})
+			.end();
+	},
+	'change enum option': browser => {
+		browser
+			.url('https://www.reddit.com/#res:settings/accountSwitcher')
+			.waitForElementVisible('#RESConsoleContainer')
+
+			// initial state, no options changed
+			.assert.cssClassPresent('#moduleOptionsSave', 'optionsSaved', 'options not staged')
+			.perform(() => {
+				browser.expect.element('#dropDownStyle-0').selected;
+			})
+
+			// select "simple arrow" dropdown style
+			.click('#dropDownStyle-1')
+			.assert.cssClassNotPresent('#moduleOptionsSave', 'optionsSaved', 'options staged')
+
+			// click save
+			.click('#moduleOptionsSave')
+			.assert.cssClassPresent('#moduleOptionsSave', 'optionsSaved', 'options not staged')
+
+			// refresh and ensure that the option was saved
+			.refresh()
+			.waitForElementVisible('#RESConsoleContainer')
+			.perform(() => {
+				browser.expect.element('#dropDownStyle-1').selected;
+			})
+			.end();
+	},
+	'change text option': browser => {
+		browser
+			.url('https://www.reddit.com/#res:settings/quickMessage')
+			.waitForElementVisible('#RESConsoleContainer')
+
+			// initial state, no options changed
+			.assert.cssClassPresent('#moduleOptionsSave', 'optionsSaved', 'options not staged')
+
+			// set a value for defaultSubject
+			.setValue('#defaultSubject', ['test subject'])
+			.assert.cssClassNotPresent('#moduleOptionsSave', 'optionsSaved', 'options staged')
+
+			// click save
+			.click('#moduleOptionsSave')
+			.assert.cssClassPresent('#moduleOptionsSave', 'optionsSaved', 'options not staged')
+
+			// refresh and ensure that the option was saved
+			.refresh()
+			.waitForElementVisible('#RESConsoleContainer')
+			.assert.value('#defaultSubject', 'test subject')
+			.end();
+	},
+	'change table option': browser => {
+		browser
+			.url('https://www.reddit.com/#res:settings/accountSwitcher')
+			.waitForElementVisible('#RESConsoleContainer')
+
+			// initial state, no options changed
+			.assert.cssClassPresent('#moduleOptionsSave', 'optionsSaved', 'options not staged')
+
+			// add row
+			.click('#optionContainer-accountSwitcher-accounts .addRowButton')
+			.setValue('#accounts_accountSwitcherUsername_1', ['test'])
+			.assert.cssClassNotPresent('#moduleOptionsSave', 'optionsSaved', 'options staged')
+
+			// click save
+			.click('#moduleOptionsSave')
+			.assert.cssClassPresent('#moduleOptionsSave', 'optionsSaved', 'options not staged')
+
+			// refresh and ensure that the option was saved
+			.refresh()
+			.waitForElementVisible('#RESConsoleContainer')
+			.assert.value('#accounts_accountSwitcherUsername_0', 'test')
 			.end();
 	},
 };
