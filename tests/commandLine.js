7761cd01d8cafef05ba7c911c304173829c43453
Erik <erikdesjardins@users.noreply.github.com>
2017-01-09 23:19:58 -0500

    skip tests in firefox

--- tests/commandLine.js
+++ tests/commandLine.js
@@ -1,5 +1,11 @@
 module.exports = {
 	'basic functionality': browser => {
+		if (browser.options.desiredCapabilities.browserName === 'firefox') {
+			// geckodriver doesn't support elementSendKeys https://github.com/mozilla/geckodriver/issues/159
+			browser.end();
+			return;
+		}
+
 		browser
 			.url('https://en.reddit.com/wiki/pages/')
 			.waitForElementVisible('#RESSettingsButton')
