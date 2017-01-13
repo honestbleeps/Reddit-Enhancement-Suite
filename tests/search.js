01bd90c2c0bf29010b607508874aaa4c557dc7e3
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2017-01-13 17:18:44 -0500

    search: fix exporting links to modules (#3889)

--- tests/search.js
+++ tests/search.js
@@ -24,4 +24,35 @@ module.exports = {
 			.assert.containsText('#SearchRES-results', 'Test Environment')
 			.end();
 	},
+	'exporting links': browser => {
+		if (browser.options.desiredCapabilities.browserName === 'firefox') {
+			// geckodriver doesn't support moveto https://github.com/mozilla/geckodriver/issues/159
+			browser.end();
+			return;
+		}
+
+		browser
+			// to modules
+			.url('https://en.reddit.com/wiki/pages/#res:settings/search/showImages')
+			.waitForElementVisible('#SearchRES-results')
+			.moveToElement('.SearchRES-result-item', 0, 0)
+			.click('.SearchRES-result-copybutton')
+			.assert.valueContains(
+				'#alert_message textarea',
+				'**[Inline Image Viewer](#res:settings/showImages)** -- [](#gear) [RES settings console](#res:settings) > Productivity > [Inline Image Viewer](#res:settings/showImages "showImages")',
+				'links to modules'
+			)
+			// to options
+			.url('https://en.reddit.com/wiki/pages/#res:settings/search/testEnvironment')
+			.refresh()
+			.waitForElementVisible('#SearchRES-results')
+			.moveToElement('.SearchRES-result-item', 0, 0)
+			.click('.SearchRES-result-copybutton')
+			.assert.valueContains(
+				'#alert_message textarea',
+				'**[testEnvironment](#res:settings/troubleshooter/testEnvironment)** -- [](#gear) [RES settings console](#res:settings) > About RES > [Troubleshooter](#res:settings/troubleshooter "troubleshooter") > [testEnvironment](#res:settings/troubleshooter/testEnvironment)',
+				'links to options'
+			)
+			.end();
+	},
 };
