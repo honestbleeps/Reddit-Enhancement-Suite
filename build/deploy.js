7b59d20938c7b0346aaea9c58fa672356a52561f
Erik <erikdesjardins@users.noreply.github.com>
2017-02-14 21:31:11 -0500

    automate Edge deployment

--- build/deploy.js
+++ build/deploy.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const chromeDeploy = require('chrome-extension-deploy');
+const edgeDeploy = require('edge-extension-deploy');
 const firefoxDeploy = require('firefox-extension-deploy');
 const { version } = require('../package.json');
 const isBetaVersion = require('./isBetaVersion');
@@ -11,12 +12,16 @@ if (isBetaVersion(version)) {
 	console.log(`Deploying ${version} beta release...`);
 
 	deployChromeBeta();
+	deployEdgeInternalBeta();
 	deployFirefoxBeta();
 } else {
 	console.log(`Deploying ${version} stable release...`);
 
 	deployChromeBeta();
 	deployChromeStable();
+	deployEdgeInternalBeta();
+	deployEdgePublicBeta();
+	deployEdgeStable();
 	deployFirefoxBeta();
 	deployFirefoxStable();
 }
@@ -52,6 +57,59 @@ function deployChromeStable() {
 		console.log('Chrome stable deployment complete!');
 	}, err => {
 		console.error('Chrome stable failed:', err);
+		process.exitCode = 1;
+	});
+}
+
+function deployEdgeInternalBeta() {
+	console.log('Deploying Edge internal beta...');
+
+	edgeDeploy({
+		tenantId: process.env.EDGE_TENANT_ID,
+		clientId: process.env.EDGE_CLIENT_ID,
+		clientSecret: process.env.EDGE_CLIENT_SECRET,
+		appId: '9NBLGGH4NC12',
+		flightId: '013e109b-ff0c-42d5-ba6a-8797ecc5368a',
+		appx: fs.createReadStream(path.join(__dirname, '../dist/edgeextension/package/edgeExtension.appx')),
+	}).then(() => {
+		console.log('Edge internal beta deployment complete!');
+	}, err => {
+		console.log('Edge internal beta failed:', err);
+		process.exitCode = 1;
+	});
+}
+
+function deployEdgePublicBeta() {
+	console.log('Deploying Edge public beta...');
+
+	edgeDeploy({
+		tenantId: process.env.EDGE_TENANT_ID,
+		clientId: process.env.EDGE_CLIENT_ID,
+		clientSecret: process.env.EDGE_CLIENT_SECRET,
+		appId: '9NBLGGH4NC12',
+		flightId: '9be3ca4c-a87f-49d2-9191-3aa40c2c9d19',
+		appx: fs.createReadStream(path.join(__dirname, '../dist/edgeextension/package/edgeExtension.appx')),
+	}).then(() => {
+		console.log('Edge public beta deployment complete!');
+	}, err => {
+		console.log('Edge public beta failed:', err);
+		process.exitCode = 1;
+	});
+}
+
+function deployEdgeStable() {
+	console.log('Deploying Edge stable...');
+
+	edgeDeploy({
+		tenantId: process.env.EDGE_TENANT_ID,
+		clientId: process.env.EDGE_CLIENT_ID,
+		clientSecret: process.env.EDGE_CLIENT_SECRET,
+		appId: '9NBLGGH4NC12',
+		appx: fs.createReadStream(path.join(__dirname, '../dist/edgeextension/package/edgeExtension.appx')),
+	}).then(() => {
+		console.log('Edge stable deployment complete!');
+	}, err => {
+		console.log('Edge stable failed:', err);
 		process.exitCode = 1;
 	});
 }
