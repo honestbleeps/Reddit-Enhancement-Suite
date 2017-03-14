c17d66af64fc08a7d8a4e58f04a65a6fe8782474
Erik <erikdesjardins@users.noreply.github.com>
2017-03-14 00:29:43 -0400

    firefox: embedded webextension

--- firefox/environment.js
+++ firefox/environment.js
@@ -1,19 +1,7 @@
-import { createMessageHandler } from '../lib/environment/common/messaging';
-
-const {
-	_handleMessage,
-	sendMessage,
-	sendSynchronous,
-	addListener,
-	addInterceptor,
-} = createMessageHandler(obj => self.postMessage(obj));
-
-self.on('message', obj => _handleMessage(obj));
+import { sendMessage, sendSynchronous, addListener } from '../chrome/environment';
 
 export {
 	sendMessage,
 	sendSynchronous,
 	addListener,
 };
-
-addInterceptor('permissions', () => true);
