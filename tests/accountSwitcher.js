573d9128e90dbf8f4049b734ff816fca3ac99014
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2017-04-01 17:40:51 -0400

    Improve account switcher error reporting (#4161)
    * improve account switcher error reporting
    
    Previously, we assumed that if an error code could not be found in the message, login was successful. This fails when reddit changes their internal error codes (which happened).
    Now, we check if `success` is true, and if we do not recognise the error, still alert the user instead of doing nothing.
    
    * test for accountswitcher error

--- tests/accountSwitcher.js
+++ tests/accountSwitcher.js
@@ -23,4 +23,28 @@ module.exports = {
 			.assert.containsText('#RESAccountSwitcherDropdown', 'add account')
 			.end();
 	},
+	'errors on invalid username/password': browser => {
+		if (browser.options.desiredCapabilities.browserName === 'firefox') {
+			// marionette crashes on setValue
+			browser.end();
+			return;
+		}
+
+		const username = 'this_username_is_too_long';
+
+		browser
+			.url('https://www.reddit.com/wiki/pages#res:settings/accountSwitcher')
+			.waitForElementVisible('#RESConsoleContainer')
+			.click('#optionContainer-accountSwitcher-accounts .addRowButton')
+			.setValue('#optionContainer-accountSwitcher-accounts input', [username])
+			.click('#moduleOptionsSave')
+
+			.url('https://en.reddit.com/r/RESIntegrationTests/wiki/pages')
+			.waitForElementVisible('#RESAccountSwitcherIcon')
+			.click('#RESAccountSwitcherIcon')
+			.click('#RESAccountSwitcherDropdown .accountName')
+			.waitForElementVisible('#alert_message')
+			.assert.containsText('#alert_message', `Could not log in as ${username}`)
+			.end();
+	},
 };
