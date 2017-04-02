3e4757d3143e021e6cd34021a46070c7bb7397f1
Erik <erikdesjardins@users.noreply.github.com>
2017-04-02 19:11:30 -0400

    basic test for commentNavigator

---
+++ tests/commentNavigator.js
@@ -0,0 +1,23 @@
+module.exports = {
+	'basic functionality': browser => {
+		if (browser.options.desiredCapabilities.browserName === 'firefox') {
+			// geckodriver doesn't support elementSendKeys https://github.com/mozilla/geckodriver/issues/159
+			browser.end();
+			return;
+		}
+
+		browser
+			.url('https://www.reddit.com/r/RESIntegrationTests/comments/632wur/comment_navigator/')
+			.waitForElementVisible('#RESSettingsButton')
+			.moveToElement('.debuginfo', 0, 0) // scroll to bottom
+			.keys(['n'])
+			.assert.visible('#REScommentNavBox')
+			.setValue('#commentNavBy', 'submitter')
+			.assert.containsText('#REScommentNavBox', '1/2')
+			.assert.visible('#thing_t1_dfqvawk')
+			.click('#commentNavDown')
+			.assert.containsText('#REScommentNavBox', '2/2')
+			.assert.visible('#thing_t1_dfqvaz1')
+			.end();
+	},
+};
