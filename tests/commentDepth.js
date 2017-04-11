87139e16deafe4d00e25e56105c2333c14e4ed8d
Erik <erikdesjardins@users.noreply.github.com>
2017-04-11 18:53:17 -0400

    commentDepth tests: pause for a second to avoid potential firefox flakiness

--- tests/commentDepth.js
+++ tests/commentDepth.js
@@ -9,17 +9,20 @@ module.exports = {
 			.waitForElementVisible('.thing.link')
 			.click('.thing.link .comments')
 			.waitForElementVisible('.thing.link')
+			.pause(1000)
 			.assert.urlEquals('https://www.reddit.com/r/RESIntegrationTests/comments/64tk5y/comment_depth_insufficient_comments/')
 			// link with sufficient comments
 			.url('https://www.reddit.com/by_id/t3_64thjt')
 			.waitForElementVisible('.thing.link')
 			.click('.thing.link .comments')
 			.waitForElementVisible('.thing.link')
+			.pause(1000)
 			.assert.urlEquals('https://www.reddit.com/r/RESIntegrationTests/comments/64thjt/comment_depth/?depth=4')
 			// link with its own depth specified
 			.waitForElementVisible('.thing.link')
 			.click('.thing.link .md a')
 			.waitForElementVisible('.thing.link')
+			.pause(1000)
 			.assert.urlEquals('https://www.reddit.com/r/RESIntegrationTests/comments/64thjt/comment_depth/?depth=10000')
 			.end();
 	},
