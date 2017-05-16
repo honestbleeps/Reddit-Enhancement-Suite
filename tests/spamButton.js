5419768b491b4544b09a3aa349a6bae77889e736
Ben McGarry <BenMcGarry@users.noreply.github.com>
2017-05-16 20:51:22 +0100

    Spam button for /r/spam closure (#4242)

--- tests/spamButton.js
+++ tests/spamButton.js
@@ -6,7 +6,7 @@ module.exports = {
 			.click('.moduleToggle')
 			.url('https://www.reddit.com/r/RESIntegrationTests/comments/5ui5yr/spam_button/')
 			.waitForElementVisible('#siteTable')
-			.assert.attributeContains('.thing.link .buttons a.option', 'href', '/r/spam/submit?url=https://www.reddit.com/user/erikdesjardins')
+			.assert.attributeContains('.thing.link .buttons a.option', 'href', '/message/compose?to=reddit.com&subject=spam&message=https://www.reddit.com/user/erikdesjardins')
 			.end();
 	},
 };
