f223cc7eedaff39eb6e77e983d78be64a1e17b95
Erik <erikdesjardins@users.noreply.github.com>
2017-06-04 02:28:14 -0400

    multicast: add crossIncognito option

--- browser/background.js
+++ browser/background.js
@@ -49,11 +49,11 @@ addListener('i18n', locale => getLocaleDictionary(locale));
 
 // Chakra bug https://github.com/Microsoft/ChakraCore/issues/2606
 // eslint-disable-next-line arrow-body-style
-addListener('multicast', async (request, { id: tabId, incognito }) => {
+addListener('multicast', async ({ name, args, crossIncognito }, { id: tabId, incognito }) => {
 	return Promise.all(
 		(await apiToPromise(chrome.tabs.query)({ url: '*://*.reddit.com/*', status: 'complete' }))
-			.filter(tab => tab.id !== tabId && tab.incognito === incognito)
-			.map(({ id: tabId }) => sendMessage('multicast', request, tabId))
+			.filter(tab => tab.id !== tabId && (crossIncognito || tab.incognito === incognito))
+			.map(({ id: tabId }) => sendMessage('multicast', { name, args }, tabId))
 	);
 });
 
