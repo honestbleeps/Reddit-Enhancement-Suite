35f9943b512ad66c03bdab71173e595c5fddaba8
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2017-06-12 19:06:04 -0400

    emulate auth flow in Chrome to fix logout after browser restart (#4300)
    ...this also allows us to use res.com as the redirect URL for all hosts.

--- browser/utils/auth.js
+++ browser/utils/auth.js
@@ -3,50 +3,85 @@
 
 import { apiToPromise } from './api';
 
-export async function emulateAuthFlow({
-	domain,
-	clientId,
-	scope,
-	interactive,
-	currentTabIndex,
-}: {|
-	domain: string,
-	clientId: string,
-	scope: string,
-	interactive: boolean,
-	currentTabIndex: number,
-|}) {
-	const redirectUri = 'https://redditenhancementsuite.com/oauth';
-	const url = new URL(domain);
-	url.searchParams.set('client_id', clientId);
-	url.searchParams.set('scope', scope);
-	url.searchParams.set('response_type', 'token');
-	url.searchParams.set('redirect_uri', redirectUri);
-
-	let id;
-	if (interactive) {
-		({ tabs: [{ id }] } = await apiToPromise(chrome.windows.create)({
-			url: url.href,
-			type: 'popup',
-		}));
-	} else {
-		({ id } = await apiToPromise(chrome.tabs.create)({
-			url: url.href,
-			index: currentTabIndex + 1,
-			active: false,
-		}));
-	}
+export async function emulateAuthFlowInNewWindow(url: string, redirectUri: string): Promise<string> {
+	// Emulate interactive auth.
+	// Open a popup window, then track its progress with chrome.tabs,
+	// succeeding if it navigates to our redirect URL, and failing if it's
+	// closed before then.
+	const { tabs: [{ id }] } = await apiToPromise(chrome.windows.create)({ url, type: 'popup' });
 
 	return new Promise((resolve, reject) => {
 		function updateListener(tabId, updates) {
 			if (tabId !== id) return;
 
 			if (updates.url && updates.url.startsWith(redirectUri)) {
-				// we've reached the redirect URL
 				stopListening();
 				resolve(updates.url);
 				apiToPromise(chrome.tabs.remove)(id);
-			} else if (!interactive && updates.status && updates.status === 'complete') {
+			}
+		}
+
+		function removeListener(tabId) {
+			if (tabId !== id) return;
+			stopListening();
+			reject(new Error('User cancelled or denied access.'));
+		}
+
+		function stopListening() {
+			chrome.tabs.onUpdated.removeListener(updateListener);
+			chrome.tabs.onRemoved.removeListener(removeListener);
+		}
+
+		chrome.tabs.onUpdated.addListener(updateListener);
+		chrome.tabs.onRemoved.addListener(removeListener);
+	});
+}
+
+export function emulateAuthFlowInBackground(url: string): Promise<string> {
+	// Emulate noninteractive auth.
+	// Fetch the auth page. If the user is preauthorized, we will 302
+	// to the redirect URL. However, because the token is passed in the hash,
+	// and fetch/XHR responses don't include the hash, we must use the
+	// webRequest API to read the redirect URL.
+	return new Promise((resolve, reject) => {
+		function headersListener({ redirectUrl }) {
+			stopListening();
+			resolve(redirectUrl);
+		}
+
+		function stopListening() {
+			chrome.webRequest.onBeforeRedirect.removeListener(headersListener);
+		}
+
+		chrome.webRequest.onBeforeRedirect.addListener(headersListener, { urls: [url] });
+
+		fetch(url, { credentials: 'include' })
+			.then(() => {
+				stopListening();
+				reject(new Error('User interaction is required.'));
+			}, e => {
+				stopListening();
+				reject(new Error(`Authorization page could not be loaded: ${e.message}`));
+				console.error(e);
+			});
+	});
+}
+
+export async function emulateAuthFlowInNewTab(url: string, redirectUri: string, currentTabIndex: number): Promise<string> {
+	// Emulate noninteractive auth.
+	// Open a background tab and track its progress with chrome.tabs,
+	// succeeding if it navigates to the redirect URL immediately.
+	const { id } = await apiToPromise(chrome.tabs.create)({ url, index: currentTabIndex + 1, active: false });
+
+	return new Promise((resolve, reject) => {
+		function updateListener(tabId, updates) {
+			if (tabId !== id) return;
+
+			if (updates.url && updates.url.startsWith(redirectUri)) {
+				stopListening();
+				resolve(updates.url);
+				apiToPromise(chrome.tabs.remove)(id);
+			} else if (updates.status && updates.status === 'complete') {
 				// the page has loaded but we haven't redirected
 				stopListening();
 				reject(new Error('User interaction is required.'));
@@ -56,7 +91,6 @@ export async function emulateAuthFlow({
 
 		function removeListener(tabId) {
 			if (tabId !== id) return;
-			// tab closed
 			stopListening();
 			reject(new Error('User cancelled or denied access.'));
 		}
--- edge/background.entry.js
+++ edge/background.entry.js
@@ -3,15 +3,29 @@
 /* eslint-env webextensions */
 
 import { addListener } from '../browser/background';
-import { emulateAuthFlow } from '../browser/utils/auth';
+import { emulateAuthFlowInNewWindow, emulateAuthFlowInNewTab } from '../browser/utils/auth';
 
 // Edge doesn't have history.*
 addListener('addURLToHistory', () => {});
 addListener('isURLVisited', () => false);
 
-addListener('authFlow', ({ domain, clientId, scope, interactive }, { index }) =>
-	emulateAuthFlow({ domain, clientId, scope, interactive, currentTabIndex: index })
-);
+addListener('authFlow', ({ domain, clientId, scope, interactive }, { index: currentIndex }) => {
+	// Edge does not support chrome.identity.launchAuthFlow.
+	// Its chrome.webRequest API does not support requests made by extensions,
+	// so we can't emulate noninteractive auth without a new tab.
+	const redirectUri = 'https://redditenhancementsuite.com/oauth';
+	const url = new URL(domain);
+	url.searchParams.set('client_id', clientId);
+	url.searchParams.set('scope', scope);
+	url.searchParams.set('response_type', 'token');
+	url.searchParams.set('redirect_uri', redirectUri);
+
+	if (interactive) {
+		return emulateAuthFlowInNewWindow(url.href, redirectUri);
+	} else {
+		return emulateAuthFlowInNewTab(url.href, redirectUri, currentIndex);
+	}
+});
 
 // see chrome/background.entry.js
 addListener('download', ({ url, filename }) => {
