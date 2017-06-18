17feb2ac52b583f7560c0994c88246969901b5fa
Erik <erikdesjardins@users.noreply.github.com>
2017-06-18 00:46:43 -0400

    Flow upgrade

--- flow/lib/dom.js
+++ flow/lib/dom.js
@@ -1,15 +1,5 @@
 /* @flow */
 
-type MutationObserverInit = {
-	childList?: boolean,
-	attributes?: boolean,
-	characterData?: boolean,
-	subtree?: boolean,
-	attributeOldValue?: boolean,
-	characterDataOldValue?: boolean,
-	attributeFilter?: Array<string>,
-};
-
 // Make event.target an HTMLElement
 // this is unsound for `window.addEventListener`
 // but it's better than having to downcast every single event target
@@ -31,14 +21,6 @@ declare class Event {
 	AT_TARGET: number;
 	BUBBLING_PHASE: number;
 	CAPTURING_PHASE: number;
-}
-
-// https://github.com/facebook/flow/blob/6d110710af4d6200ff8bf510ce6438aa203de09b/lib/dom.js#L1598
-// Only <option> and <optgroup> can inhabit this in valid HTML
-declare class HTMLOptionsCollection {
-	length: number;
-	item(index: number): HTMLOptionElement;
-	namedItem(name: string): HTMLOptionElement;
 }
 
 // https://github.com/facebook/flow/blob/7b172c807fea56f8dc8f40f4539e683fa16e4b2f/lib/bom.js#L824
