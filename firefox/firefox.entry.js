21c69bb574c4bb9160e34ca79620945b77274ad6
Erik <erikdesjardins@users.noreply.github.com>
2017-07-15 22:38:23 -0400

    Remove URLSearchParams shim in Firefox

--- firefox/firefox.entry.js
+++ firefox/firefox.entry.js
@@ -3,14 +3,3 @@
 /* Firefox-specific polyfills */
 
 import 'intersection-observer';
-
-// Firefox doesn't support objects in URLSearchParams constructor (until Firefox 54)
-// https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams#Browser_compatibility
-window.URLSearchParams = class URLSearchParams extends window.URLSearchParams {
-	constructor(init) {
-		if (typeof init === 'object' && !Array.isArray(init)) {
-			init = Object.entries(init);
-		}
-		super(init);
-	}
-};
