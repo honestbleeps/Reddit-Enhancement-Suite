c49fa645867ca070adc4b6825f322c9ac02af989
Lars <larsjohnsen@gmail.com>
2017-07-26 23:40:50 +0200

    Polyfill for Edge 15 IntersectionObserverEntry.prototype.isIntersecting

--- edge/edge.entry.js
+++ edge/edge.entry.js
@@ -14,3 +14,14 @@ window.chrome = window.browser; // eslint-disable-line no-native-reassign
 // DOM Collection iteration
 NodeList.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
 HTMLCollection.prototype[Symbol.iterator] = Array.prototype[Symbol.iterator];
+
+// polyfill IntersectionObserverEntry.prototype.isIntersecting
+// Snippet from https://github.com/WICG/IntersectionObserver/pull/224
+if (!('isIntersecting' in IntersectionObserverEntry.prototype)) {
+	Object.defineProperty(IntersectionObserverEntry.prototype, // eslint-disable-line prefer-reflect
+		'isIntersecting', {
+			get() {
+				return this.intersectionRatio > 0;
+			},
+		});
+}
