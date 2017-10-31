5b9d35938438295690554b4dae601ee225bf5d60
kavishka1 <33263867+kavishka1@users.noreply.github.com>
2017-10-31 11:00:20 -0700

    update.build.token.js

--- build/buildToken.js
+++ build/buildToken.js
@@ -1,6 +1,4 @@
-/* @flow */
 
-/* eslint-disable import/no-commonjs, import/no-nodejs-modules */
 
 // used for invalidating caches on each build (executed at build time)
 module.exports = String(Math.random()).slice(2);
