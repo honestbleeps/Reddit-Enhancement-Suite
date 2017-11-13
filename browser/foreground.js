9d6c666f13ebe0cf7317fdb51f3df0765a2f7ed3
Erik <erikdesjardins@users.noreply.github.com>
2017-11-13 14:55:39 -0500

    storage: implement BlobWrapper
    Like PrefixWrapper, but stores everything under one object. Provides easier deletion/enumeration of keys, faster getAll, with slower get and set.

--- browser/foreground.js
+++ browser/foreground.js
@@ -60,6 +60,8 @@ addInterceptor('storage', keyedMutex(async ([operation, key, value]) => {
 		case 'setMultiple':
 			return _set(value);
 		case 'patch':
+			return set(key, Object.assign(await get(key) || {}, value));
+		case 'patchDeep':
 			const extended = extendDeep(await get(key) || {}, value);
 			return set(key, extended);
 		case 'deletePath':
