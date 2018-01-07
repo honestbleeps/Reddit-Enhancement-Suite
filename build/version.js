4b5e58efbfa82d30da68ff82f7dada1253ae5590
Erik <erikdesjardins@users.noreply.github.com>
2018-01-07 17:14:06 -0500

    automate changelog post deployment

--- build/version.js
+++ build/version.js
@@ -3,14 +3,13 @@
 /* eslint-disable import/no-commonjs, import/no-nodejs-modules */
 
 const { execSync } = require('child_process');
-const path = require('path');
 const fs = require('fs');
 const { version, repository } = require('../package.json');
+const { changelogPath, changelogPathFromVersion } = require('./utils/changelog');
 
-const dir = 'changelog';
 const unreleasedChangelog = changelogPath('UNRELEASED.md');
 const templateChangelog = changelogPath('_EXAMPLE.md');
-const newChangelog = changelogPath(`v${version}.md`);
+const newChangelog = changelogPathFromVersion(version);
 
 const changelogContents = fs.readFileSync(unreleasedChangelog, { encoding: 'utf8' });
 fs.writeFileSync(newChangelog, `## [v${version}](https://github.com/${repository.username}/${repository.repository}/releases/v${version})\n\n${changelogContents}`);
@@ -19,10 +18,6 @@ fs.writeFileSync(unreleasedChangelog, fs.readFileSync(templateChangelog));
 
 gitAdd(unreleasedChangelog);
 gitAdd(newChangelog);
-
-function changelogPath(filename) {
-	return path.resolve(__dirname, path.join('..', dir, filename));
-}
 
 function gitAdd(file) {
 	console.log('git add', file);
