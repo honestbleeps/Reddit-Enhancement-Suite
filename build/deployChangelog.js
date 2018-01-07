ebffb7ba9b9e703f58b2674e2deddbc56b2344c8
Erik <erikdesjardins@users.noreply.github.com>
2018-01-07 18:18:16 -0500

    add .git to repo urls

--- build/deployChangelog.js
+++ build/deployChangelog.js
@@ -13,7 +13,7 @@ const isBetaVersion = require('./isBetaVersion');
 const tempDir = path.join(__dirname, '..', 'dist', 'temp');
 
 rimraf.sync(tempDir);
-execSync(`git clone --depth=1 https://github.com/Reddit-Enhancement-Suite/Reddit-Enhancement-Suite.github.io ${tempDir}`);
+execSync(`git clone --depth=1 https://github.com/Reddit-Enhancement-Suite/Reddit-Enhancement-Suite.github.io.git ${tempDir}`);
 
 const releaseTimestamp = (/^tagger.+?\s(\d+)\s/m).exec(execSync(`git cat-file tag v${version}`, { encoding: 'utf8' }))[1];
 const releaseDate = new Date(releaseTimestamp * 1000);
@@ -40,6 +40,6 @@ fs.writeFileSync(newChangelogFile, newChangelog);
 
 execSync(`git add ${newChangelogFile}`, { cwd: tempDir });
 execSync(`git commit --author="${process.env.BOT_USER} <${process.env.BOT_USER}@users.noreply.github.com>" -m "changelog v${version}"`, { cwd: tempDir });
-execSync(`git push https://${process.env.BOT_USER}:${process.env.BOT_PASSWORD}@github.com/Reddit-Enhancement-Suite/Reddit-Enhancement-Suite.github.io`, { cwd: tempDir });
+execSync(`git push https://${process.env.BOT_USER}:${process.env.BOT_PASSWORD}@github.com/Reddit-Enhancement-Suite/Reddit-Enhancement-Suite.github.io.git`, { cwd: tempDir });
 
 console.log('Deployed changelog post:', newChangelogFile);
