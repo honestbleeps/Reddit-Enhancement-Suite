e09755da3acb02958e315c899565f1785faa8d32
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2018-08-30 13:01:40 -0400

    remove @babel/plugin-proposal-export-default-from (#4840)
    Flow can't parse this syntax so we can't use it anyways.
    
    Also, the equivalent syntax:
    `export Foo from './foo'` -> `export { default as Foo } from './foo'`
    isn't much longer and is clearer IMO (i.e. does the first one export something called `Foo` or the default export?)

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -74,7 +74,6 @@ export default (env = {}, argv = {}) => {
 						loader: 'babel-loader',
 						options: {
 							plugins: [
-								'@babel/plugin-proposal-export-default-from',
 								'@babel/plugin-proposal-export-namespace-from',
 								['@babel/plugin-proposal-class-properties', { loose: true }],
 								['@babel/plugin-proposal-object-rest-spread', { loose: true, useBuiltIns: true }],
