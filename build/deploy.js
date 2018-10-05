fc6f2ca0759410d272b28728786615065743b163
Mayank Khandelwal <mayankkhandelwal92@gmail.com>
2018-10-06 01:28:10 +0530

    Indented code

--- build/deploy.js
+++ build/deploy.js
@@ -12,120 +12,120 @@ const { version } = require('../package.json');
 const isBetaVersion = require('./isBetaVersion');
 
 if (isBetaVersion(version)) {
-	console.log(`Deploying ${version} beta release...`);
+				console.log(`Deploying ${version} beta release...`);
 
-	deployChromeBeta();
-	deployEdgeBeta();
+				deployChromeBeta();
+				deployEdgeBeta();
 } else {
-	console.log(`Deploying ${version} stable release...`);
+				console.log(`Deploying ${version} stable release...`);
 
-	deployChromeBeta();
-	deployChromeStable();
-	deployEdgeBeta();
-	deployEdgeStable();
-	deployFirefoxStable();
-	deployOperaStable();
+				deployChromeBeta();
+				deployChromeStable();
+				deployEdgeBeta();
+				deployEdgeStable();
+				deployFirefoxStable();
+				deployOperaStable();
 }
 
 function deployChromeBeta() {
-	console.log('Deploying Chrome beta...');
+				console.log('Deploying Chrome beta...');
 
-	chromeDeploy({
-		clientId: process.env.CHROME_CLIENT_ID,
-		clientSecret: process.env.CHROME_CLIENT_SECRET,
-		refreshToken: process.env.CHROME_REFRESH_TOKEN,
-		id: 'flhpapomijliefifkkeepedibpmibbpo',
-		zip: fs.readFileSync(path.join(__dirname, '../dist/zip/chrome-beta.zip')),
-		to: chromeDeploy.TRUSTED_TESTERS,
-	}).then(() => {
-		console.log('Chrome beta deployment complete!');
-	}, err => {
-		console.error('Chrome beta failed:', err);
-		process.exitCode = 1;
-	});
+				chromeDeploy({
+								clientId: process.env.CHROME_CLIENT_ID,
+								clientSecret: process.env.CHROME_CLIENT_SECRET,
+								refreshToken: process.env.CHROME_REFRESH_TOKEN,
+								id: 'flhpapomijliefifkkeepedibpmibbpo',
+								zip: fs.readFileSync(path.join(__dirname, '../dist/zip/chrome-beta.zip')),
+								to: chromeDeploy.TRUSTED_TESTERS,
+				}).then(() => {
+								console.log('Chrome beta deployment complete!');
+				}, err => {
+								console.error('Chrome beta failed:', err);
+								process.exitCode = 1;
+				});
 }
 
 function deployChromeStable() {
-	console.log('Deploying Chrome stable...');
+				console.log('Deploying Chrome stable...');
 
-	chromeDeploy({
-		clientId: process.env.CHROME_CLIENT_ID,
-		clientSecret: process.env.CHROME_CLIENT_SECRET,
-		refreshToken: process.env.CHROME_REFRESH_TOKEN,
-		id: 'kbmfpngjjgdllneeigpgjifpgocmfgmb',
-		zip: fs.readFileSync(path.join(__dirname, '../dist/zip/chrome.zip')),
-	}).then(() => {
-		console.log('Chrome stable deployment complete!');
-	}, err => {
-		console.error('Chrome stable failed:', err);
-		process.exitCode = 1;
-	});
+				chromeDeploy({
+								clientId: process.env.CHROME_CLIENT_ID,
+								clientSecret: process.env.CHROME_CLIENT_SECRET,
+								refreshToken: process.env.CHROME_REFRESH_TOKEN,
+								id: 'kbmfpngjjgdllneeigpgjifpgocmfgmb',
+								zip: fs.readFileSync(path.join(__dirname, '../dist/zip/chrome.zip')),
+				}).then(() => {
+								console.log('Chrome stable deployment complete!');
+				}, err => {
+								console.error('Chrome stable failed:', err);
+								process.exitCode = 1;
+				});
 }
 
 function deployEdgeBeta() {
-	console.log('Deploying Edge beta...');
+				console.log('Deploying Edge beta...');
 
-	edgeDeploy({
-		tenantId: process.env.EDGE_TENANT_ID,
-		clientId: process.env.EDGE_CLIENT_ID,
-		clientSecret: process.env.EDGE_CLIENT_SECRET,
-		appId: '9NBLGGH4NC12',
-		flightId: '9be3ca4c-a87f-49d2-9191-3aa40c2c9d19',
-		appx: fs.createReadStream(path.join(__dirname, '../dist/edgeextension/package/edgeExtension.appx')),
-	}).then(() => {
-		console.log('Edge beta deployment complete!');
-	}, err => {
-		console.log('Edge beta failed:', err);
-		process.exitCode = 1;
-	});
+				edgeDeploy({
+								tenantId: process.env.EDGE_TENANT_ID,
+								clientId: process.env.EDGE_CLIENT_ID,
+								clientSecret: process.env.EDGE_CLIENT_SECRET,
+								appId: '9NBLGGH4NC12',
+								flightId: '9be3ca4c-a87f-49d2-9191-3aa40c2c9d19',
+								appx: fs.createReadStream(path.join(__dirname, '../dist/edgeextension/package/edgeExtension.appx')),
+				}).then(() => {
+								console.log('Edge beta deployment complete!');
+				}, err => {
+								console.log('Edge beta failed:', err);
+								process.exitCode = 1;
+				});
 }
 
 function deployEdgeStable() {
-	console.log('Deploying Edge stable...');
+				console.log('Deploying Edge stable...');
 
-	edgeDeploy({
-		tenantId: process.env.EDGE_TENANT_ID,
-		clientId: process.env.EDGE_CLIENT_ID,
-		clientSecret: process.env.EDGE_CLIENT_SECRET,
-		appId: '9NBLGGH4NC12',
-		appx: fs.createReadStream(path.join(__dirname, '../dist/edgeextension/package/edgeExtension.appx')),
-	}).then(() => {
-		console.log('Edge stable deployment complete!');
-	}, err => {
-		console.log('Edge stable failed:', err);
-		process.exitCode = 1;
-	});
+				edgeDeploy({
+								tenantId: process.env.EDGE_TENANT_ID,
+								clientId: process.env.EDGE_CLIENT_ID,
+								clientSecret: process.env.EDGE_CLIENT_SECRET,
+								appId: '9NBLGGH4NC12',
+								appx: fs.createReadStream(path.join(__dirname, '../dist/edgeextension/package/edgeExtension.appx')),
+				}).then(() => {
+								console.log('Edge stable deployment complete!');
+				}, err => {
+								console.log('Edge stable failed:', err);
+								process.exitCode = 1;
+				});
 }
 
 function deployFirefoxStable() {
-	console.log('Deploying Firefox stable...');
+				console.log('Deploying Firefox stable...');
 
-	firefoxDeploy({
-		issuer: process.env.FIREFOX_ISSUER,
-		secret: process.env.FIREFOX_SECRET,
-		id: 'jid1-xUfzOsOFlzSOXg@jetpack',
-		version: require('../dist/firefox/manifest.json').version, // eslint-disable-line global-require
-		src: fs.createReadStream(path.join(__dirname, '../dist/zip/firefox.zip')),
-	}).then(() => {
-		console.log('Firefox stable deployment complete!');
-	}, err => {
-		console.error('Firefox stable failed:', err);
-		process.exitCode = 1;
-	});
+				firefoxDeploy({
+								issuer: process.env.FIREFOX_ISSUER,
+								secret: process.env.FIREFOX_SECRET,
+								id: 'jid1-xUfzOsOFlzSOXg@jetpack',
+								version: require('../dist/firefox/manifest.json').version, // eslint-disable-line global-require
+								src: fs.createReadStream(path.join(__dirname, '../dist/zip/firefox.zip')),
+				}).then(() => {
+								console.log('Firefox stable deployment complete!');
+				}, err => {
+								console.error('Firefox stable failed:', err);
+								process.exitCode = 1;
+				});
 }
 
 function deployOperaStable() {
-	console.log('Deploying Opera stable...');
+				console.log('Deploying Opera stable...');
 
-	operaDeploy({
-		username: process.env.OPERA_USER,
-		password: process.env.OPERA_PASSWORD,
-		id: '53435',
-		zip: fs.readFileSync(path.join(__dirname, '../dist/zip/chrome.zip')),
-	}).then(() => {
-		console.log('Opera stable deployment complete!');
-	}, err => {
-		console.error('Opera stable failed:', err);
-		process.exitCode = 1;
-	});
+				operaDeploy({
+								username: process.env.OPERA_USER,
+								password: process.env.OPERA_PASSWORD,
+								id: '53435',
+								zip: fs.readFileSync(path.join(__dirname, '../dist/zip/chrome.zip')),
+				}).then(() => {
+								console.log('Opera stable deployment complete!');
+				}, err => {
+								console.error('Opera stable failed:', err);
+								process.exitCode = 1;
+				});
 }
