c4a28c27fe87aff9fc8aebd6a3a9c0aa66cc162e
Lars <larsjohnsen@gmail.com>
2020-09-13 23:40:41 +0200

    Fix test/typing

--- tests/userTagger.js
+++ tests/userTagger.js
@@ -18,32 +18,32 @@ module.exports = {
 		browser
 			// tag icon appears correctly
 			.url('https://en.reddit.com/r/RESIntegrationTests/comments/5sgqzh/user_tagger/?limit=1')
-			.waitForElementVisible(tag(post), 'post icon tag visible')
-			.waitForElementVisible(tag(parentComment), 'parent comment tag icon visible')
+			.waitForElementVisible('css selector', tag(post), 'post icon tag visible')
+			.waitForElementVisible('css selector', tag(parentComment), 'parent comment tag icon visible')
 			.click(loadChildComment)
-			.waitForElementVisible(tag(childComment), 'child comment tag icon visible')
+			.waitForElementVisible('css selector', tag(childComment), 'child comment tag icon visible')
 
 			// tags work for newly loaded comments
 			.refresh()
-			.waitForElementVisible(tag(post))
+			.waitForElementVisible('css selector', tag(post))
 			.click(tag(post))
 			.assert.visible('#userTaggerToolTip')
 			.setValue('#userTaggerToolTip #userTaggerText', ['test tag'])
 			.click('#userTaggerSave')
 			.click(loadChildComment)
-			.waitForElementVisible(tag(childComment))
+			.waitForElementVisible('css selector', tag(childComment))
 			.assert.containsText(tag(childComment), 'test tag')
 
 			// works in post listings
 			.url('https://en.reddit.com/by_id/t3_5sgqzh')
-			.waitForElementVisible(tag(post))
+			.waitForElementVisible('css selector', tag(post))
 			.assert.containsText(tag(post), 'test tag')
 
 			// cli
 			.keys(['.'])
 			.assert.visible('#keyCommandLineWidget')
 			.keys(['tag tag via cli'])
-			.waitForElementVisible('#keyCommandInputTip:not(:empty)')
+			.waitForElementVisible('css selector', #keyCommandInputTip:not(:empty)')
 			.keys([browser.Keys.ENTER])
 			.assert.containsText(tag(post), 'tag via cli')
 			.end();
