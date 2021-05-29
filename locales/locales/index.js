a0ae0aef3c5c51f72cb6be9b7dd9e8dd5cc7201a
Lars <larsjohnsen@gmail.com>
2021-05-29 12:19:24 +0200

    update deps

--- locales/locales/index.js
+++ locales/locales/index.js
@@ -1,22 +1,22 @@
 /* @flow */
 /* eslint-disable camelcase */
 
-import de from './de';
-import el from './el';
-import en from './en';
-import en_lolcat from './en@lolcat';
-import en_pirate from './en@pirate';
-import en_CA from './en_CA';
-import en_GB from './en_GB';
-import es from './es';
-import es_419 from './es_419';
-import he from './he';
-import it from './it';
-import nl_NL from './nl_NL';
-import pl from './pl';
-import pt from './pt';
-import pt_BR from './pt_BR';
-import pt_PT from './pt_PT';
+import de from './de.json';
+import el from './el.json';
+import en from './en.json';
+import en_lolcat from './en@lolcat.json';
+import en_pirate from './en@pirate.json';
+import en_CA from './en_CA.json';
+import en_GB from './en_GB.json';
+import es from './es.json';
+import es_419 from './es_419.json';
+import he from './he.json';
+import it from './it.json';
+import nl_NL from './nl_NL.json';
+import pl from './pl.json';
+import pt from './pt.json';
+import pt_BR from './pt_BR.json';
+import pt_PT from './pt_PT.json';
 
 export default {
 	de,
