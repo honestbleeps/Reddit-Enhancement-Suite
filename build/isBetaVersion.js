cc2293d2f4bdf136592d9a1eb43b28258ef7d0ec
removedporn <86824510+removedporn@users.noreply.github.com>
2021-07-24 19:08:03 +0800

    Delete build directory

--- build/isBetaVersion.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-
-/* eslint-disable import/no-commonjs */
-
-const semver = require('semver');
-
-module.exports = function isBetaVersion(version /*: string */) /*: boolean */ {
-	return (semver.minor(version) % 2) === 1;
-};
