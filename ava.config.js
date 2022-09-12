6b64f89e7af6bf7c577179c336b0ae7e39359418
Lars <larsjohnsen@gmail.com>
2022-09-12 17:16:01 +0200

    Add missing dependency esm

--- ava.config.js
+++ ava.config.js
@@ -3,12 +3,11 @@
 export default {
 	files: [
 		'**/__tests__/*.js',
-		'!lib/modules/backupAndRestore/__tests__/serialization.js',
 		// Ignore tests which breaks due to unsupported runtime
+		'!lib/modules/backupAndRestore/__tests__/serialization.js',
 		'!lib/utils/__tests__/string.js',
 		'!lib/utils/__tests__/async.js',
 		'!lib/utils/__tests__/array.js',
-		'!lib/utils/__tests__/location.js',
 	],
 	require: [
 		'@babel/register',
