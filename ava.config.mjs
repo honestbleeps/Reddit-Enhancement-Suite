c149300ade91702527fc58f2ed1e8417afa86592
Lars Johnsen <larsjohnsen@gmail.com>
2024-01-15 19:01:27 +0100

    fix ava test
    This removes ava's broken dependency transpiling using babel. Instead it strips the flow type definitions, and runs the ava tests on native code.
    
    Running the code natively also requires explitic extensions on imports, so '.js' is added where necessary.

--- ava.config.mjs
+++ ava.config.mjs
@@ -2,14 +2,7 @@
 
 export default {
 	files: [
-		'**/__tests__/*.js',
-		// Ignore tests which breaks due to unsupported runtime
-		'!lib/modules/backupAndRestore/__tests__/serialization.js',
-		'!lib/utils/__tests__/string.js',
-		'!lib/utils/__tests__/async.js',
-		'!lib/utils/__tests__/array.js',
-	],
-	require: [
-		'@babel/register',
+		// `lib` must be transpiled before tests are executed
+		'dist/transpiled/**/__tests__/*.js',
 	],
 };
